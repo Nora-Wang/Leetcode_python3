1295394513340b0b750c5108e9c83f39ca22d08a
Nora_Wang <ywang41@gwmail.gwu.edu>
2019-09-03 17:54:00 -0400

    Create 141. Linked List Cycle.py

---
+++ ListNode/141. Linked List Cycle.py
@@ -0,0 +1,45 @@
+题目:
+Given a linked list, determine if it has a cycle in it.
+
+To represent a cycle in the given linked list, we use an integer pos which represents the position (0-indexed) in the linked list where tail connects to. If pos is -1, then there is no cycle in the linked list.
+
+ 
+
+Example 1:
+
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where tail connects to the second node.
+
+思路：
+一个快指针一个慢指针，如果路径上有环，快慢指针一定会相遇
+
+快慢指针模板:
+slow = fast = head
+while(fast and fast.next):
+  slow = slow.next
+  fast = fast.next.next
+
+
+code:
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        slow = fast = head
+        while(fast and fast.next):
+            slow = slow.next
+            fast = fast.next.next
+            if(fast == slow):
+                return True
+        return False
+       
