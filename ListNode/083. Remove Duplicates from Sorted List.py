d542c909d5ab7987a01c2eceea97596e953ec219
Nora_Wang <ywang41@gwmail.gwu.edu>
2019-09-05 19:25:19 -0400

    Update 083. Remove Duplicates from Sorted List.py

--- ListNode/083. Remove Duplicates from Sorted List.py
+++ ListNode/083. Remove Duplicates from Sorted List.py
@@ -15,7 +15,7 @@ Output: 1->2->3
 设置一个ListNode curt = head的头指针
 若curt.val = curt.next.val则将curt的指针指向curt.next.next，即将curt.next这个node从head中删掉
 
-
+###第二次做没考虑[]的情况。两种方法。
 
 
 code:
@@ -32,10 +32,12 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
+   ###第一种方法
         if(head == None):
             return None
         curt = head
-        
+  ###第二年种方法
+        #while(curt and curt.next)
         while(curt.next != None):
             if(curt.val != curt.next.val):
                 curt = curt.next
