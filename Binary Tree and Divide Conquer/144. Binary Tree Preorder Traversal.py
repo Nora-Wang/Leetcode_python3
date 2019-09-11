e203ced1af8de3b92ff3dfb4c2579c9c59d719de
Nora_Wang <ywang41@gwmail.gwu.edu>
2019-09-11 16:35:54 -0400

    Update and rename Preorder.py to 144. Binary Tree Preorder Traversal.py

--- Binary Tree and Divide Conquer/144. Binary Tree Preorder Traversal.py
+++ Binary Tree and Divide Conquer/144. Binary Tree Preorder Traversal.py
@@ -1,3 +1,30 @@
+题目：
+Given a binary tree, return the preorder traversal of its nodes' values.
+
+Example:
+
+Input: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+Output: [1,2,3]
+Follow up: Recursive solution is trivial, could you do it iteratively?
+
+
+思路：
+1.递归法recursive
+
+首先遍历根节点，然后对其左节点做前序遍历，对其右节点做前序遍历。
+
+递归三要素：
+
+定义：要做什么事情。这道题就是先遍历父亲节点（将root加入到result[]中），然后左节点、右节点
+拆分：差分成同样的问题，但规模变小。本题就是拆成左子树和右子树，对左子树和右字数分别做前序遍历
+结束条件：规模变为最小的情况。本题为遇到空节点停止。
+
 # Version 0: Recursion 
 """
 Definition of TreeNode:
@@ -11,18 +38,28 @@ class Solution:
     @param root: The root of binary tree.
     @return: Preorder in ArrayList which contains node values.
     """
+    
     def preorderTraversal(self, root):
         self.results = []
         self.traverse(root)
         return self.results
         
     def traverse(self, root):
+        结束条件
         if root is None:
             return
+        定义
         self.results.append(root.val)
+        拆分
         self.traverse(root.left)
         self.traverse(root.right)
 
+        
+        
+2.非递归：迭代 Non-Recursive: Iteration
+
+利用stack实现树的前序遍历，每次弹出栈顶元素，先后压入其右孩子和左孩子
+    
 # Version 1: Non-Recursion  
 """
 Definition of TreeNode:
