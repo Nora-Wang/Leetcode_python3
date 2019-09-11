c2ea3a5814427a8f7a3af55834b0f2c256b79196
Nora_Wang <ywang41@gwmail.gwu.edu>
2019-09-11 16:47:16 -0400

    Update 144. Binary Tree Preorder Traversal.py

--- Binary Tree and Divide Conquer/144. Binary Tree Preorder Traversal.py
+++ Binary Tree and Divide Conquer/144. Binary Tree Preorder Traversal.py
@@ -23,7 +23,7 @@ Follow up: Recursive solution is trivial, could you do it iteratively?
 
 定义：要做什么事情。这道题就是先遍历父亲节点（将root加入到result[]中），然后左节点、右节点
 拆分：差分成同样的问题，但规模变小。本题就是拆成左子树和右子树，对左子树和右字数分别做前序遍历
-结束条件：规模变为最小的情况。本题为遇到空节点停止。
+结束条件：规模变为最小的情况。本题为遇到空节点停止
 
 # Version 0: Recursion 
 """
@@ -45,12 +45,12 @@ class Solution:
         return self.results
         
     def traverse(self, root):
-        结束条件
+#结束条件：本题为遇到空节点停止
         if root is None:
             return
-        定义
+#定义：这道题就是先遍历父亲节点（将root加入到result[]中），然后左节点、右节点
         self.results.append(root.val)
-        拆分
+#拆分本题就是拆成左子树和右子树，对左子树和右字数分别做前序遍历
         self.traverse(root.left)
         self.traverse(root.right)
 
@@ -76,13 +76,20 @@ class Solution:
     def preorderTraversal(self, root):
         if root is None:
             return []
+        #根入栈
         stack = [root]
         preorder = []
         while stack:
             node = stack.pop()
             preorder.append(node.val)
+            #右节点入栈
             if node.right:
+                stack.append(node.right)
+            #左节点入栈
                 stack.append(node.right)
             if node.left:
                 stack.append(node.left)
         return preorder
+   
+######注意：先让右节点入栈，再让左节点入栈。
+因为栈的特点是先进后出，栈每次pop的都是栈顶，所以当同存在左右节点时，需要先右后左，这样pop时就是先左后右，以满足preorder的要求
