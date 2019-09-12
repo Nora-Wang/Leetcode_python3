08ce582dcb0d38ed6a7377807c2d0f40d71b6d6a
Nora_Wang <ywang41@gwmail.gwu.edu>
2019-09-12 11:33:42 -0400

    Create Minimum Subtree.py

---
+++ Binary Tree and Divide Conquer/Minimum Subtree.py
@@ -0,0 +1,32 @@
+题目：
+Given a binary tree, find the subtree with minimum sum. Return the root of the subtree.
+
+
+divide conquer + traverse
+
+class Solution:
+    def findSubtree(self, root):
+    #注意刚开始的min_sum的取值，系统最大值
+      self.min_sum = sys.maxsize
+      self.min_sum_root = None
+      findsum(root)
+      
+      return self.min_sum_root
+    
+    def findsum(self,root):
+      if not root:
+        return None
+        
+      #divide conquer
+      left = findsum(root.left)
+      right = findsum(root.right)
+      cur_sum = left.val + right.val + root.val
+      
+      #traverse
+      if(cur_sum < self.min_sum):
+        self.min_sum = cur_sum
+        self.min_sum_root = root
+        
+      #个人觉得return ,也行
+      return cur_sum
+        
