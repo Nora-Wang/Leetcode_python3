7bd06aabcc8e83e388ae63cd49adfd5e95d78d57
Nora_Wang <ywang41@gwmail.gwu.edu>
2019-09-12 09:48:54 -0400

    Update 145. Binary Tree Postorder Traversal.py

--- Binary Tree and Divide Conquer/145. Binary Tree Postorder Traversal.py
+++ Binary Tree and Divide Conquer/145. Binary Tree Postorder Traversal.py
@@ -43,4 +43,35 @@ class Solution(object):
         
         
 2.Non-recursive
+终于找到一个简单易懂的Non-recursive方法
+利用两个栈，在s1中按照根左右的顺序push进node，然后将右node pop出，再循环；即相当于将整个结果反着push进s2，这样s2 pop时的顺序就是postorder的顺序
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
+class Solution(object):
+    def postorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root:
+            return []
+        result = []
+        s1 = [root]
+        s2 = []
+        while s1:
+            node = s1.pop()
+            s2.append(node)
+            
+            if node.left:
+                s1.append(node.left)
+            if node.right:
+                s1.append(node.right)
+            
+        while s2:
+            result.append(s2.pop().val)
+        return result
