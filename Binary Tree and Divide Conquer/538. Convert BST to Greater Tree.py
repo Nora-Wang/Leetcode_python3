25aa7925c799911d1e358999890b0334ac2d7223
Nora_Wang <ywang41@gwmail.gwu.edu>
2019-09-14 12:12:35 -0400

    Update 538. Convert BST to Greater Tree.py

--- Binary Tree and Divide Conquer/538. Convert BST to Greater Tree.py
+++ Binary Tree and Divide Conquer/538. Convert BST to Greater Tree.py
@@ -37,6 +37,10 @@ class Solution(object):
         if not root:
             return None
         ##注意写法，用的是root.right和root.left
+        #原因：
+        #root.right这个递归，是从根一直往右下走，找到最右下角的叶子位置，这个子节点是整棵树不变的那个节点
+        #这道题的解法主要是要按照降序的方法去遍历这个bst，并且伴随着遍历的过程，我们要维持一个当前的sum，这个sum记录所有visited nodes的和
+      ###所以遍历这个bst其实就是一个反转in-order traversal的遍历，即右根左的顺序遍历整个bst
         root.right = self.convertBST(root.right)
         
         self.sum += root.val
