91700781e140fa15e96d339f5344ce281defe537
Nora_Wang <ywang41@gwmail.gwu.edu>
2019-10-08 10:51:42 -0400

    Update 207. Course Schedule.py

--- Breadth First Search/207. Course Schedule.py
+++ Breadth First Search/207. Course Schedule.py
@@ -22,6 +22,8 @@ Explanation: There are a total of 2 courses to take.
 ############################################################
 topological sort：判读一个有向图，是无环的
 queue和list都是append，set是add
+set用于在graph中，用node去映射sun_node(neighbors)，这时的sun_node(neighbors)是set类型
+原因：set判断一个node在不在set里时，可以直接找到有没有这个node；而list则需要从头遍历的寻找
 ############################################################
 
 解题思路：
