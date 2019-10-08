4f181ddaae05959be71f0f53f6f68c9ef5a587c6
Nora_Wang <ywang41@gwmail.gwu.edu>
2019-10-08 08:39:09 -0400

    Update 207. Course Schedule.py

--- Breadth First Search/207. Course Schedule.py
+++ Breadth First Search/207. Course Schedule.py
@@ -26,9 +26,9 @@ topological sort：判读一个有向图，是无环的
 解题思路：
 这就是一个问图能否topological sort的问题，但是因为所给信息的形式是node + edges的形式，因此我们还需要先将其转换为graph字典
 
-数据类型设计：graph{'node':'neighbor'}, indegrees{'node':'indegree_count'}；其中neighbor的数据类型为set，indegree_count为int
-########这里要注意理解，后续的indegrees的设计是当发现node有一个neighbor时，它的入度+1
-????????
+数据类型设计：graph{'sub_node':'node'}, indegrees{'node':'indegree_count'}；其中node的数据类型为set，indegree_count为int
+########！！！！这里要注意理解graph的设计，因为后续的indegrees的设计是当发现sub_node有一个node时，sub_node的入度+1；起始node的indegreess=0
+
 
 step1:创建graph
 step2:计算每个node的indegree数量
@@ -59,11 +59,11 @@ class Solution(object):
         while queue:
             node = queue.popleft()
             result.append(node)
-            #将node pop出后，其对应的neighbor的indegree直接-1，若该neighbor的indegree此时为0，则其应为此时的起始node，因此需将其加入queue
-            for neighbor in graph[node]:
-                indegrees[neighbor] -= 1
-                if indegrees[neighbor] == 0:
-                    queue.append(neighbor)
+            #将node pop出后，其对应的sub_node的indegree直接-1，若该sub_node的indegree此时为0，则其应为此时的起始node，因此需将其加入queue
+            for sub_node in graph[node]:
+                indegrees[sub_node] -= 1
+                if indegrees[sub_node] == 0:
+                    queue.append(sub_node)
         #若无环，则所有的node都应该在result里
         if len(result) == numCourses:
             return True
@@ -77,7 +77,9 @@ class Solution(object):
         for node in range(numCourses):
             graph[node] = set()
         for edge in prerequisites:
-        #参考题目中数据的设计[[0,1]]=take course 0 you have to first take course 1
+########参考题目中数据的设计[[0,1]]=take course 0 you have to first take course 1;即graph[1]=0,对于course1来说，它的值为course0
+########这样便于后续indegrees的计算，graph[sub_node]的value有多少个，它的indegree就为多少
+########对于起始node，它的value=None
             sub_course, course = edge[0], edge[1]
             graph[sub_course].add(course)
         return graph
@@ -86,9 +88,9 @@ class Solution(object):
         indegrees = {}
         for node in graph:
             indegrees[node] = 0
-        for node in graph:
-            for neighbor in graph[node]:
-                indegrees[neighbor] += 1
+        for sub_node in graph:
+            for node in graph[sub_node]:
+                indegrees[node] += 1
         return indegrees
         
     
