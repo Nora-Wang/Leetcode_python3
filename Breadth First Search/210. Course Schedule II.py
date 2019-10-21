a13b42ac69a2420462bf736f10d3345299cb862f
Nora_Wang <ywang41@gwmail.gwu.edu>
2019-10-21 16:32:59 -0400

    Update 210. Course Schedule II.py

--- Breadth First Search/210. Course Schedule II.py
+++ Breadth First Search/210. Course Schedule II.py
@@ -23,12 +23,62 @@ Explanation: There are a total of 4 courses to take. To take course 3 you should
              
              
              
-主题思路跟207一模一样，只是最后的输出topological sorting的结果，若有环，则输出None
-
-此题有个注意点：在graph的创建上跟207一样，但这道题需要result.reverse()
+主题思路跟207一模一样，只是I用count记录node个数，若与numCourses相同即符合
+而II则需要用result记录所遍历的courses，若其长度与numCourses相同即符合，然后输出result
 
 code:
+class Solution:
+    """
+    @param: numCourses: a total of n courses
+    @param: prerequisites: a list of prerequisite pairs
+    @return: the course order
+    """
+    def findOrder(self, numCourses, prerequisites):
+        #step 1
+        graph, indegrees = self.create_graph_indegrees(numCourses, prerequisites)
+        
+        queue = collections.deque([])
+        #step 2
+        #此处可用range(numCourses)，也可用graph
+        for node in range(numCourses):
+            if indegrees[node] == 0:
+                queue.append(node)
+        result = []
+        while queue:
+            node = queue.popleft()
+            result.append(node)
+            for neighbor in graph[node]:
+                indegrees[neighbor] -= 1
+                if indegrees[neighbor] == 0:
+                    queue.append(neighbor)
+        if len(result) == numCourses:
+            return result
+        else:
+            return []
+        
+    def create_graph_indegrees(self, numCourses, prerequisites):
+        graph = {}
+        indegrees = []
+        for course in range(numCourses):
+            graph[course] = set()
+            indegrees.append(0)
+        for edge in prerequisites:
+            sub_course = edge[0]
+            course = edge[1]
+       #####注意点！！避免重复计算indegrees
+            if sub_course not in graph[course]:
+                graph[course].add(sub_course)
+                indegrees[sub_course] += 1
+        return graph, indegrees
 
+
+
+
+
+
+Boss版本
+此版本有个注意点：在graph的创建上跟207一样，但这道题需要result.reverse()，因为Boss的逻辑是反的，所以结果也是反的
+code:
 class Solution(object):
     def findOrder(self, numCourses, prerequisites):
         """
