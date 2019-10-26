d7fba938ca7f0c64303804f7e42ba9916da5fa53
Nora_Wang <ywang41@gwmail.gwu.edu>
2019-10-26 18:06:02 -0400

    Update 153. Find Minimum in Rotated Sorted Array.py

--- Binary Search/153. Find Minimum in Rotated Sorted Array.py
+++ Binary Search/153. Find Minimum in Rotated Sorted Array.py
@@ -93,6 +93,32 @@ class Solution(object):
                     
         
 lintcode version
+version 1更简洁:
+  class Solution:
+    """
+    @param nums: a rotated sorted array
+    @return: the minimum number in the array
+    """
+    def findMin(self, nums):
+        if len(nums) == 0:
+            return None
+        
+        start = 0
+        end = len(nums) - 1
+        
+        if nums[start] < nums[end]:
+            return nums[start]
+        
+        while start + 1 < end:
+            mid = start + (end - start) / 2
+            if nums[mid] <= nums[end]:
+                end = mid
+            else:
+                start = mid
+        return min(nums[start], nums[end])
+
+      
+version 2:
 class Solution:
     """
     @param nums: a rotated sorted array
