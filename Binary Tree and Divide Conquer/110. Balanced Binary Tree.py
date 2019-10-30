f1ab3076876e484dd3f5fcee09a120abbed61e03
Nora_Wang <ywang41@gwmail.gwu.edu>
2019-10-30 17:47:57 -0400

    Update 110. Balanced Binary Tree.py

--- Binary Tree and Divide Conquer/110. Balanced Binary Tree.py
+++ Binary Tree and Divide Conquer/110. Balanced Binary Tree.py
@@ -22,11 +22,8 @@ version2用-1做标记，容易出错
 
 
 1. Version九章
-
-Class ResultType
-用ResultType类来记录depth和是否balanced
-主要运用=是divide and conquer + traverse的思想
-
+divide and conquer
+用两个值分别代表深度和平衡性
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
@@ -34,41 +31,31 @@ Class ResultType
 #         self.left = None
 #         self.right = None
 
-#使用类来标记depth和isBalanced
-class ResultType:
-    def __init__(self, depth, IsBalanced):
-        self.depth = depth
-        self.IsBalanced = IsBalanced
-        
-class Solution:
-    #直接返回在调用了MaxDepth函数后的root的IsBalanced
+class Solution(object):
     def isBalanced(self, root):
         """
         :type root: TreeNode
         :rtype: bool
         """
-        return self.MaxDepth(root).IsBalanced
+        _, isbalanced = self.maxdepth(root)
+        
+        return isbalanced
     
-    def MaxDepth(self, root):
-        #单节点情况
+    def maxdepth(self, root):
         if not root:
-            return ResultType(0, True)
-            
-        #divide and conquer
-        left = self.MaxDepth(root.left)
-        right = self.MaxDepth(root.right)
+            return 0, True
         
-        #当左节点或右节点不平衡时，以root为根的树也不是平衡二叉树
-        if left.IsBalanced == False or right.IsBalanced == False:
-            return ResultType(-1, False)
+        left_depth, left_isbalanced = self.maxdepth(root.left)
+        if not left_isbalanced:
+            return 0, False
         
-        #Balanced Binary Tree的定义：左右子树高度差小于等于1
-        if abs(left.depth - right.depth) > 1:
-            return ResultType(-1, False)
+        right_depth, right_isbalanced = self.maxdepth(root.right)
+        if not right_isbalanced:
+            return 0, False
         
-        #此处返回的是一个ResultType
-        #能使用到这一return的node就说明此时的node是处于平衡的状态，需要记录其depth，以便于后续node求depth的时候可以直接+1
-        return ResultType(max(left.depth, right.depth) + 1, True)
+        return max(left_depth, right_depth) + 1, abs(left_depth - right_depth) <= 1
+        
+
         
         
         
