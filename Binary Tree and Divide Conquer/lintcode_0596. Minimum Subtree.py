d82934d2244ee79c82c78bbc11c2d7597affc75c
Nora_Wang <ywang41@gwmail.gwu.edu>
2019-10-31 11:02:22 -0400

    Create lintcode_0596. Minimum Subtree.py

---
+++ Binary Tree and Divide Conquer/lintcode_0596. Minimum Subtree.py
@@ -0,0 +1,78 @@
+Given a binary tree, find the subtree with minimum sum. Return the root of the subtree.
+
+Example
+
+Example 1:
+
+Input:
+{1,-5,2,1,2,-4,-5}
+Output:1
+Explanation:
+The tree is look like this:
+     1
+   /   \
+ -5     2
+ / \   /  \
+1   2 -4  -5 
+The sum of whole tree is minimum, so return the root.
+Example 2:
+
+Input:
+{1}
+Output:1
+Explanation:
+The tree is look like this:
+   1
+There is one and only one subtree in the tree. So we return 1.
+Notice
+
+LintCode will print the subtree which root is your return node.
+It's guaranteed that there is only one subtree with minimum sum and the given binary tree is not an empty tree.
+
+
+思路：找出左右子树的sum、左右子树minsum及其root；计算root的sum，与左右子树的minsum做比较，返回当前sum，比较后的minsum及其root
+
+注意点：返回值的选取
+left_min_root, left_sum, left_minsum
+
+
+返回时
+
+code:
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: the root of binary tree
+    @return: the root of the minimum subtree
+    """
+    def findSubtree(self, root):
+        root,_,_ = self.minsum(root)
+        
+        return root
+        
+    def minsum(self, root):
+        if not root:
+            return root, 0, sys.maxsize
+
+        #左边最小sum子树的root，左边的sum，左边最小sum
+        left_min_root, left_sum, left_minsum= self.minsum(root.left)
+        right_min_root, right_sum, right_minsum= self.minsum(root.right)
+        
+        cur_sum = left_sum + right_sum + root.val
+        
+        min_sum = min(left_minsum, right_minsum, cur_sum)
+        
+        if min_sum == left_minsum:
+            #注意这里返回的是cur_sum，这样后续才能继续计算当前root的sum
+            return left_min_root, cur_sum, left_minsum
+        if min_sum == right_minsum:
+            return right_min_root, cur_sum, right_minsum
+            
+        return root, cur_sum, cur_sum
