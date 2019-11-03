560e04a0b7ff527c1d07440e11114c8e6a58798c
Nora_Wang <ywang41@gwmail.gwu.edu>
2019-11-02 22:16:04 -0400

    Update lintcode_0596. Minimum Subtree.py

--- Binary Tree and Divide Conquer/lintcode_0596. Minimum Subtree.py
+++ Binary Tree and Divide Conquer/lintcode_0596. Minimum Subtree.py
@@ -30,15 +30,14 @@ LintCode will print the subtree which root is your return node.
 It's guaranteed that there is only one subtree with minimum sum and the given binary tree is not an empty tree.
 
 
-思路：找出左右子树的sum、左右子树minsum及其root；计算root的sum，与左右子树的minsum做比较，返回当前sum，比较后的minsum及其root
+思路：divide and conquer + traverse
+找出左右子树的sum、左右子树minsum及其root；计算root的sum，与左右子树的minsum做比较，返回当前sum，比较后的minsum及其root
 
 注意点：返回值的选取
 left_min_root, left_sum, left_minsum
 
-
-返回时
-
 code:
+Version 1: 不使用全局变量
 """
 Definition of TreeNode:
 class TreeNode:
@@ -76,3 +75,40 @@ class Solution:
             return right_min_root, cur_sum, right_minsum
             
         return root, cur_sum, cur_sum
+
+
+Version 2:使用全局变量
+     """
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: the root of binary tree
+    @return: the root of the minimum subtree
+    """
+    def findSubtree(self, root):
+        self.subtree = None
+        self.sum_record = sys.maxsize
+        self.minsum(root)
+        
+        return self.subtree
+        
+    def minsum(self, root):
+        if not root:
+            return 0
+            
+        left_sum = self.minsum(root.left)
+        right_sum = self.minsum(root.right)
+        
+        root_sum = left_sum + right_sum + root.val
+        
+        if self.sum_record > root_sum:
+            self.sum_record = root_sum
+            self.subtree = root
+            
+        return root_sum
