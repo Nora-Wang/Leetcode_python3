2e066e758f6152e64eb45959631a4de055dd4cba
Nora_Wang <ywang41@gwmail.gwu.edu>
2019-11-07 17:53:55 -0500

    Create 026. Remove Duplicates from Sorted Array.py

---
+++ Two Pointer/026. Remove Duplicates from Sorted Array.py
@@ -0,0 +1,67 @@
+Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+Example 1:
+
+Given nums = [1,1,2],
+
+Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+
+It doesn't matter what you leave beyond the returned length.
+Example 2:
+
+Given nums = [0,0,1,1,1,2,2,3,3,4],
+
+Your function should return length = 5, with the first five elements of nums being modified to 0, 1, 2, 3, and 4 respectively.
+
+It doesn't matter what values are set beyond the returned length.
+Clarification:
+
+Confused why the returned value is an integer but your answer is an array?
+
+Note that the input array is passed in by reference, which means modification to the input array will be known to the caller as well.
+
+Internally you can think of this:
+
+// nums is passed in by reference. (i.e., without making a copy)
+int len = removeDuplicates(nums);
+
+// any modification to nums in your function would be known by the caller.
+// using the length returned by your function, it prints the first len elements.
+for (int i = 0; i < len; i++) {
+    print(nums[i]);
+}
+
+
+这个问题有两种做法:
+1. 第一种做法比较容易想到的是，把所有的数扔到 hash 表里，然后就能找到不同的整数有哪些。但是这种做法会耗费额外空间 O(n)。
+#这种做法已经不能AC
+
+2. 面试官会追问，如何不耗费额外空间。
+第二种做法用到双指针算法，首先将数组排序，这样那些重复的整数就会被挤在一起。然后用两根指针，一根指针走得快一些遍历整个数组，另外一根指针，
+一直指向当前不重复部分的最后一个数。快指针发现一个和慢指针指向的数不同的数之后，就可以把这个数丢到慢指针的后面一个位置，并把慢指针++。
+
+code:
+Version 1
+
+Version 2 two pointer
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return []
+        
+        slow, quick = 0, 1
+        
+        while quick < len(nums):
+            if nums[slow] != nums[quick]:
+                slow += 1
+                nums[slow] = nums[quick]
+            quick += 1
+            
+        return slow + 1
+            
