e6426a9692093096ab2d834b3f1a3635657f48ad
Nora_Wang <ywang41@gwmail.gwu.edu>
2019-11-07 16:22:59 -0500

    Create 001. Two Sum.py

---
+++ Two Pointer/001. Two Sum.py
@@ -0,0 +1,31 @@
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+
+
+
+
+code:
+Version 1
+使用dict，key为值，value为index
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        hash = {}
+        
+        for i in range(len(nums)):
+            if target - nums[i] in hash:
+                return [hash[target - nums[i]], i]
+            hash[nums[i]] = i
+                        
