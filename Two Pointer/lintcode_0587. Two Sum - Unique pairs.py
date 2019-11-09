f85c45464e3cebfd647477c0d9e82c75c2859839
Nora_Wang <ywang41@gwmail.gwu.edu>
2019-11-08 20:34:26 -0500

    Update lintcode_0587. Two Sum - Unique pairs.py

--- Two Pointer/lintcode_0587. Two Sum - Unique pairs.py
+++ Two Pointer/lintcode_0587. Two Sum - Unique pairs.py
@@ -22,7 +22,7 @@ Explanation:
 
 code:
 Version 1
-将result放入set，这样可以直接避免重复
+将result设为set，这样可以直接避免重复
 class Solution:
     """
     @param nums: an array of integer
@@ -50,7 +50,7 @@ class Solution:
     
     
 Version 2
-用count来记录有多少个结果，当nums[start]和nums[end]发生重复时，跳过当前值
+用count来记录有多少种结果，在nums[start] + nums[end] == target情况下，当nums[start]和nums[end]发生重复时，跳过当前值
 class Solution:
     """
     @param nums: an array of integer
@@ -81,3 +81,34 @@ class Solution:
                 end -= 1
                 
         return count
+
+Version 3
+将result设为list，因为已经排序，所以在nums[start] + nums[end] == target情况下，当nums[start]与前一个相等时，直接跳过
+class Solution:
+    """
+    @param nums: an array of integer
+    @param target: An integer
+    @return: An integer
+    """
+    def twoSum6(self, nums, target):
+        if not nums:
+            return 0
+            
+        nums.sort()
+        start, end = 0, len(nums) - 1
+        result = []
+        
+        while start < end:
+            if nums[start] + nums[end] == target:
+                if not result:
+                    result.append(nums[start])
+                elif result and nums[start] != result[-1]:
+                    result.append(nums[start])
+                start += 1
+                end -= 1
+            elif nums[start] + nums[end] < target:
+                start += 1
+            else:
+                end -= 1
+                
+        return len(result)
