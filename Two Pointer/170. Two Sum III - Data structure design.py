4ddf137fdfa8d3dc56f414109c2359ebb59eaf6f
Nora_Wang <ywang41@gwmail.gwu.edu>
2019-11-09 15:12:50 -0500

    Update 170. Two Sum III - Data structure design.py

--- Two Pointer/170. Two Sum III - Data structure design.py
+++ Two Pointer/170. Two Sum III - Data structure design.py
@@ -14,10 +14,52 @@ add(3); add(1); add(2);
 find(3) -> true
 find(6) -> false
 
-此题只能使用hash
+
 这道题的解法有3种情况
 
+
 1.要求add和find的时间复杂度较平均
+class TwoSum:
+    """
+    @param number: An integer
+    @return: nothing
+    """
+    def __init__(self):
+        self.dic = []
+        
+    #insert sort:先将number放入数组的最后一位，然后依次与前一个数做大小比较，若小，则swap
+    def add(self, number):
+        self.dic.append(number)
+        index = len(self.dic) - 1
+        
+        #while循环记得写index -= 1！！！！
+        while index > 0:
+            if self.dic[index] < self.dic[index - 1]:
+                self.dic[index - 1], self.dic[index] = self.dic[index], self.dic[index - 1]
+            index -= 1
+                
+
+    """
+    @param value: An integer
+    @return: Find if there exists any pair of numbers which sum is equal to the value.
+    """
+    #two pointer模板,value相当于target
+    def find(self, value):
+        
+        start, end = 0, len(self.dic) - 1
+        
+        while start < end:
+            if self.dic[start] + self.dic[end] == value:
+                return True
+            if self.dic[start] + self.dic[end] < value:
+                start += 1
+            else:
+                end -= 1
+                
+        return False
+      
+2.add调用的很多，而find调用的很少；即要求add的时间复杂度到最低
+这种解法的add只要O(1)
 class TwoSum:
     """
     @param number: An integer
@@ -42,8 +84,26 @@ class TwoSum:
                 return True
                 
         return False
-      
-2.add调用的很多，而find调用的很少；即要求add的时间复杂度到最低
-
 
 3.find调用的很多，而add调用的很少；即要求find的时间复杂度到最低
+这种解法的find只要O(1)
+class TwoSum:
+    """
+    @param number: An integer
+    @return: nothing
+    """
+    def __init__(self):
+        self.dic = []
+        self.sum = set()
+        
+    def add(self, number):
+        for num in self.dic:
+            self.sum.add(num + number)
+        self.dic.append(number)
+
+    """
+    @param value: An integer
+    @return: Find if there exists any pair of numbers which sum is equal to the value.
+    """
+    def find(self, value):
+        return value in self.sum
