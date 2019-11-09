8a71c1773b98bc24d59d667b6770814f5f78acf8
Nora_Wang <ywang41@gwmail.gwu.edu>
2019-11-09 14:49:54 -0500

    Update 170. Two Sum III - Data structure design.py

--- Two Pointer/170. Two Sum III - Data structure design.py
+++ Two Pointer/170. Two Sum III - Data structure design.py
@@ -18,7 +18,32 @@ find(6) -> false
 这道题的解法有3种情况
 
 1.要求add和find的时间复杂度较平均
+class TwoSum:
+    """
+    @param number: An integer
+    @return: nothing
+    """
+    def __init__(self):
+        self.dic = {}
+        
+    def add(self, number):
+        if number in self.dic:
+            self.dic[number] += 1
+        else:
+            self.dic[number] = 1
 
+    """
+    @param value: An integer
+    @return: Find if there exists any pair of numbers which sum is equal to the value.
+    """
+    def find(self, value):
+        for num in self.dic:
+            if value - num in self.dic and (value - num != num or self.dic[num] > 1):
+                return True
+                
+        return False
+      
 2.add调用的很多，而find调用的很少；即要求add的时间复杂度到最低
+
 
 3.find调用的很多，而add调用的很少；即要求find的时间复杂度到最低
