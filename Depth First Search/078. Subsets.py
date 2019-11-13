ebb75dba4b36d77ed8becc928cd5ddf58d206f7a
Nora_Wang <ywang41@gwmail.gwu.edu>
2019-11-13 13:12:40 -0500

    Update 078. Subsets.py

--- Depth First Search/078. Subsets.py
+++ Depth First Search/078. Subsets.py
@@ -44,6 +44,7 @@ class Solution(object):
         #这里为什么需要写成list(temp), temp本身不就是list吗? 
         #这里牵扯到一个copy问题，如果不加list，那么copy的就是temp的reference，因此list之后的改变都会导致之前加入值的改变，
         #加上list()之后就是建立了一个当前temp的copy，之后无论list如何改变，就不变了
+        #这新建了一个list，list的构造接受一个Iterable对象作为参数，并将该对象内的元素按顺序添加到新建的list中。这也是一次Deep Copy。
         combinations.append(list(temp))
         
         for i in range(index, len(nums)):
