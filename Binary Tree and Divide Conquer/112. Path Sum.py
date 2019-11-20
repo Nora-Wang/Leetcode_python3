fdc3fc913e31314387d419285e06f700c6858d83
Nora_Wang <ywang41@gwmail.gwu.edu>
2019-11-19 22:00:08 -0500

    Create 112. Path Sum.py

---
+++ Binary Tree and Divide Conquer/112. Path Sum.py
@@ -0,0 +1,47 @@
+Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Given the below binary tree and sum = 22,
+
+      5
+     / \
+    4   8
+   /   / \
+  11  13  4
+ /  \      \
+7    2      1
+return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
+
+
+用recursion分析不同情况
+
+code:
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def hasPathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: bool
+        """
+        if not root:
+            return False
+            
+        #叶节点时
+        elif root.val == sum and not root.left and not root.right:
+            return True
+        #recursion左子树和右子树
+        else:
+            return self.hasPathSum(root.left, sum - root.val) or self.hasPathSum(root.right, sum - root.val)
+        
+        
