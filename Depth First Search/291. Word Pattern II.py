f7ac808e85c4172cbe01701d1dfb4f6e697a2e95
Nora_Wang <ywang41@gwmail.gwu.edu>
2019-11-22 00:38:00 -0500

    Create 291. Word Pattern II.py

---
+++ Depth First Search/291. Word Pattern II.py
@@ -0,0 +1,18 @@
+Given a pattern and a string str, find if str follows the same pattern.
+
+Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty substring in str.
+
+Example 1:
+
+Input: pattern = "abab", str = "redblueredblue"
+Output: true
+Example 2:
+
+Input: pattern = pattern = "aaaa", str = "asdasdasdasd"
+Output: true
+Example 3:
+
+Input: pattern = "aabb", str = "xyzabcxzyabc"
+Output: false
+Notes:
+You may assume both pattern and str contains only lowercase letters.
