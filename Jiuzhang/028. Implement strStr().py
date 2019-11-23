96aa10adf25d825be7822d7ce072da75385f6415
Nora_Wang <ywang41@gwmail.gwu.edu>
2019-11-22 20:38:34 -0500

    Update 028. Implement strStr().py

--- Jiuzhang/028. Implement strStr().py
+++ Jiuzhang/028. Implement strStr().py
@@ -12,15 +12,11 @@ Input: haystack = "aaaaa", needle = "bba"
 Output: -1
 
 
-从第一个开始比较，若不等则移向下一个(i);注意比较的范围是0～len(haystack) - len(needle)，但是range是取左不取右
-当从i+0到i+len(needle)-1的值都相等时，则return i
-
+注意此题的corner case的判断不需要另写,因为在for循环里面就可以包括
+思路:以len(needle)长度为标准,在haystack中寻找haystack[i:len(needle) + i]
 
 
 code:
-Version 0:
-use while loop
-
 class Solution(object):
     def strStr(self, haystack, needle):
         """
@@ -28,39 +24,10 @@ class Solution(object):
         :type needle: str
         :rtype: int
         """
-        if haystack is None or needle is None:
-            return -1
         for i in range(len(haystack) - len(needle) + 1):
-            j = 0
-            while j < len(needle):
-                if haystack[i + j] != needle[j]:
-                    break
-                j += 1
-            if j == len(needle):
+            if needle == haystack[i:len(needle) + i]:
                 return i
+            
         return -1
             
-Version 1:
-use for loop
-
-class Solution(object):
-    def strStr(self, haystack, needle):
-        """
-        :type haystack: str
-        :type needle: str
-        :rtype: int
-        """
-        if len(needle) == 0:
-            return 0
-        if haystack is None or needle is None:
-            return -1
-        for i in range(len(haystack) - len(needle) + 1):
-            j = 0
-            for j in range(len(needle)):
-                if haystack[i + j] != needle[j]:
-                    break
-                else:
-                    if j == len(needle) - 1:
-                        return i
-        return -1
             
