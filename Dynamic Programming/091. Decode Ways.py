2453f66b4e9e3bc54f7e834b17cad95d8277ccff
Nora_Wang <ywang41@gwmail.gwu.edu>
2019-12-22 00:42:15 -0500

    Create 091. Decode Ways.py

---
+++ Dynamic Programming/091. Decode Ways.py
@@ -0,0 +1,65 @@
+A message containing letters from A-Z is being encoded to numbers using the following mapping:
+
+'A' -> 1
+'B' -> 2
+...
+'Z' -> 26
+Given a non-empty string containing only digits, determine the total number of ways to decode it.
+
+Example 1:
+
+Input: "12"
+Output: 2
+Explanation: It could be decoded as "AB" (1 2) or "L" (12).
+Example 2:
+
+Input: "226"
+Output: 3
+Explanation: It could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
+
+
+
+思路：
+参考DP经典题目上楼梯问题：n节楼梯从第0层开始，每次只能1层或2层，问上到第n层有 多少种方法?
+解决方案：f[n]表示从第0层走到第n层一共有多少种方法，f[n] = f[n-1] + f[n-2]，时间复杂度为O(n)
+注意：一般f[0]都为1
+
+
+此题思路同上楼梯问题：
+result[n] = result[n-1] + result[n-2]
+有result[n-2]的条件是result[n-2]存在,并且s[n-1]与s[n]组成的数字在10-26之间
+有result[n-1]的条件result[n-1]存在,并且s[n - 1]!=0
+
+eg:
+s = “12”
+n.     0 1 2
+s        1 2
+result 1 1 2
+1.n = 1时,因为不存在result[n - 2],因此不用加result[n - 2],并且由于存在result[n - 1] and s[0]不为0 -> result[1] = result[0] = 1
+2.n = 2时,因为存在result[n - 2] and 12在10~26范围内,并且由于存在result[n - 1] and s[1]不为0 -> result[2] = result[0] + result[1] = 2
+
+
+code:
+class Solution(object):
+    def numDecodings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if not s:
+            return 0
+        
+        result = [0] * (len(s) + 1)
+        result[0] = 1
+        
+        for i in range(1, len(result)):
+            #注意s取的方式
+            #由于s的index会空一个,因此其取值应该是result的index - 1(逻辑问题)
+            #s为string,需要提前int才能与数字比较大小
+            if result[i - 2] and 10 <= int(s[i - 2 : i]) <= 26:
+                result[i] += result[i - 2]
+            
+            if result[i - 1] and s[i - 1] != '0':
+                result[i] += result[i - 1]
+                
+        return result[-1]
