0894e9f0f72629603127c56dde9518f4e61da5ae
Nora_Wang <ywang41@gwmail.gwu.edu>
2020-01-10 17:52:54 -0500

    Update functions.py

--- Python Notes/functions.py
+++ Python Notes/functions.py
@@ -10,3 +10,8 @@ mid = (end - start) // 2
 4.取小数后k位
 round(num,k)
 然而,事实上,直接写1.00000/用float()更有用,参考346
+
+5.随机取值
+import random
+random.randint(start, last)
+random.sample(data, 5)  #从data中随机获取5个元素，作为一个片断返回  
