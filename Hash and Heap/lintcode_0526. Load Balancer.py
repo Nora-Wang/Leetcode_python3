81c9b9966eab4412cde0d65d203133d8cc1a2cd9
Nora_Wang <ywang41@gwmail.gwu.edu>
2020-01-10 18:13:57 -0500

    Update lintcode_0526. Load Balancer.py

--- Hash and Heap/lintcode_0526. Load Balancer.py
+++ Hash and Heap/lintcode_0526. Load Balancer.py
@@ -31,10 +31,9 @@ Output:
   3
 Explanation: The return value of pick() is random, it can be either 2 3 3 1 3 2 2 or other.
 
-思路:这题直接用set就行.
-难点就是如何随机选取数据:调用random package
 
 code:
+高频班version:直接用set
 class LoadBalancer:
     def __init__(self):
         self.cluster = set()
@@ -70,3 +69,48 @@ class LoadBalancer:
         #random.sample(序列a，n):从序列a(str/list/set)中随机抽取n个元素，并将n个元素生以list形式返回
         #random.sample(序列a，n)[0]返回生成的随机数列表中的第一个值
         return random.sample(self.cluster,1)[0]
+
+      
+      
+follow up:如何在不使用set的情况下用O(1)的时间实现？
+
+算法班Version:参考380,使用list+dict的方式,能在不用set的前提下实现O(1)
+import random
+class LoadBalancer:
+    def __init__(self):
+        self.data = []
+        self.record = {}
+
+    """
+    @param: server_id: add a new server to the cluster
+    @return: nothing
+    """
+    def add(self, server_id):
+        if server_id in self.record:
+            return
+        
+        self.data.append(server_id)
+        self.record[server_id] = len(self.data) - 1
+
+    """
+    @param: server_id: server_id remove a bad server from the cluster
+    @return: nothing
+    """
+    def remove(self, server_id):
+        if server_id not in self.record:
+            return
+        
+        index = self.record[server_id]
+        last = self.data[-1]
+        
+        self.data[index] = last
+        self.record[last] = index
+        
+        self.data.pop()
+        del self.record[server_id]
+
+    """
+    @return: pick a server in the cluster randomly with equal probability
+    """
+    def pick(self):
+        return self.data[random.randint(0, len(self.data) - 1)]
