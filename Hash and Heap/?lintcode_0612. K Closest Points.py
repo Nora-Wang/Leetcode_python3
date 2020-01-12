12e640ab2c844f38f9a2ae187aca87fe538968ba
Nora_Wang <ywang41@gwmail.gwu.edu>
2020-01-12 16:43:14 -0500

    Create ?lintcode_0612. K Closest Points.py

---
+++ Hash and Heap/?lintcode_0612. K Closest Points.py
@@ -0,0 +1,16 @@
+Given some points and an origin point in two-dimensional space, find k points which are nearest to the origin.
+Return these points sorted by distance, if they are same in distance, sorted by the x-axis, and if they are same in the x-axis, sorted by y-axis.
+
+Example
+
+Example 1:
+
+Input: points = [[4,6],[4,7],[4,4],[2,5],[1,1]], origin = [0, 0], k = 3 
+Output: [[1,1],[2,5],[4,4]]
+Example 2:
+
+Input: points = [[0,0],[0,9]], origin = [3, 1], k = 1
+Output: [[0,0]]
+
+
+code:
