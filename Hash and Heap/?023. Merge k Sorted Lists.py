443b8fefe58b52212ee6c5b9c1bde27ff03dd080
Nora_Wang <ywang41@gwmail.gwu.edu>
2020-01-12 16:50:21 -0500

    Create ?023. Merge k Sorted Lists.py

---
+++ Hash and Heap/?023. Merge k Sorted Lists.py
@@ -0,0 +1,21 @@
+Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+
+Example:
+
+Input:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+Output: 1->1->2->3->4->4->5->6
+
+
+code:
+#Version 1
+
+
+#Version 2
+
+
+#Version 3
