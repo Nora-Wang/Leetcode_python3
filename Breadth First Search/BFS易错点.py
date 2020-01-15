a604642422b28565fe2b7a4688d72a61b93a8e3a
Nora_Wang <ywang41@gwmail.gwu.edu>
2020-01-15 15:07:32 -0500

    Update BFS易错点.py

--- Breadth First Search/BFS易错点.py
+++ Breadth First Search/BFS易错点.py
@@ -2,8 +2,9 @@
 
 1.用了grid[new_x][new_y] = 1就不用设置self.visited了
 
-2.有count/step/level就一定要有for _ in range(len_level),注意初始值的设置;并且一定要放在pop的后面
-#注意将这里的count与word ladder区分一下;knight那道题的count初始值为0,因为这里需要的是path length;而word ladder需要的是word的个数
+2.有count/step/level就一定要有for _ in range(len_level),它们的初始值都设为0
+如何判断应该放在while queue中的最上面还是最下面:如果需要的结果是node的个数,则是上面;如果需要的是边数(即变化次数),则是下面
+
 
 3.注意题的source和destination是Point类型,使用的时候需要source.x,source.y
 
