50ea62c960affeaf03cbc3d6607502a4358bdd00
Nora_Wang <ywang41@gwmail.gwu.edu>
2020-01-17 22:03:08 -0500

    Update 538. Convert BST to Greater Tree.py

--- Binary Tree and Divide Conquer/538. Convert BST to Greater Tree.py
+++ Binary Tree and Divide Conquer/538. Convert BST to Greater Tree.py
@@ -49,3 +49,39 @@ class Solution(object):
         root.left = self.convertBST(root.left)
         
         return root
+      
+      
+      
+更新版本:
+inorder倒过来即可,需要设置一个self.sum全局变量来记录最近所得的sum,而不能直接用root.val,因为其root不一定为需要的那个值,
+如下eg:4那个地方的值应该是13+5+4,而4的root却为2
+              5
+            /   \
+           2     13
+            \
+             4
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def __init__(self):
+        self.sum = 0
+        
+    def convertBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if not root:
+            return
+        
+        self.convertBST(root.right)
+        root.val += self.sum
+        self.sum = root.val
+        self.convertBST(root.left)
+        
+        return root
