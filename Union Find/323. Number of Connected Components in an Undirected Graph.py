c6fad64dc3422b266d7f6cf15c078b97e0495c09
Nora_Wang <ywang41@gwmail.gwu.edu>
2020-01-27 15:41:47 -0500

    Create 323. Number of Connected Components in an Undirected Graph.py

---
+++ Union Find/323. Number of Connected Components in an Undirected Graph.py
@@ -0,0 +1,79 @@
+Given n nodes labeled from 0 to n - 1 and a list of undirected edges (each edge is a pair of nodes), write a function to find the number of connected components in an undirected graph.
+
+Example 1:
+
+Input: n = 5 and edges = [[0, 1], [1, 2], [3, 4]]
+
+     0          3
+     |          |
+     1 --- 2    4 
+
+Output: 2
+Example 2:
+
+Input: n = 5 and edges = [[0, 1], [1, 2], [2, 3], [3, 4]]
+
+     0           4
+     |           |
+     1 --- 2 --- 3
+
+Output:  1
+Note:
+You can assume that no duplicate edges will appear in edges. Since all edges are undirected, [0, 1] is the same as [1, 0] and thus will not appear together in edges.
+
+
+典型Union Find的题目
+
+
+
+code:
+class Solution(object):
+    def countComponents(self, n, edges):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :rtype: int
+        """
+        #corner case
+        if not edges:
+            return n
+        
+        #初始化父集,使所有的点的父亲都指向自己
+        self.point_to_father = {}
+        for i in range(n):
+            self.point_to_father[i] = i
+            
+        #对于存在边的每个点做并查集
+        for edge in edges:
+            self.union(edge[0], edge[1])
+        
+        #找到作为根的节点的个数,就是连通图的个数
+        count = 0
+        for point in self.point_to_father:
+            if self.point_to_father[point] == point:
+                count += 1
+                
+        return count
+    
+    #Union set
+    #当发现相连的两个点的根结点不同时,将其中一个直接化为另一个的子树
+    def union(self, point_a, point_b):
+        root_a = self.find(point_a)
+        root_b = self.find(point_b)
+        
+        if root_a != root_b:
+            self.point_to_father[root_a] = root_b
+    
+    #Find set + Compressed path
+    #记录当前点走到根结点的路径,然后将所经过的所有点的根节点都记录为最终根节点
+    def find(self, point):
+        path = []
+        
+        while self.point_to_father[point] != point:
+            path.append(point)
+            point = self.point_to_father[point]
+        
+        for p in path:
+            self.point_to_father[p] = point
+        
+        return point
