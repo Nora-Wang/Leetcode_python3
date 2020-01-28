b3afae60792768177b35325b1019cde4f8a54e93
Nora_Wang <ywang41@gwmail.gwu.edu>
2020-01-27 21:20:14 -0500

    Update 721. Accounts Merge.py

--- Union Find/721. Accounts Merge.py
+++ Union Find/721. Accounts Merge.py
@@ -22,3 +22,77 @@ The length of accounts[i][j] will be in the range [1, 30].
 
 
 code:
+class Solution(object):
+    def accountsMerge(self, accounts):
+        """
+        :type accounts: List[List[str]]
+        :rtype: List[List[str]]
+        """
+        self.father = {}
+        #初始化把所有的account中的email都指向自己
+        self.initialize(accounts)
+        
+        #把每个account中的email都连起来（12连，23连，34连，45连） 
+        for account in accounts:
+            for i in range(1,len(account) - 1):
+                self.union(account[i], account[i + 1])
+                
+                
+        #设置email_set来存{email的根: 对应的子email} 
+        #设置email_to_acct来存{email的根: 对应的姓名name} 
+        email_set, email_to_id = self.get_id_to_email_set(accounts)
+        
+        #从email_set和email_to_acct中取结果 
+        result = []
+        for root_email in email_set:
+            temp = [email_to_id[root_email]] + sorted(list(email_set[root_email]))
+            result.append(temp)
+            
+        return result
+    
+    def get_id_to_email_set(self, accounts):
+        email_set = {}
+        email_to_id = {}
+        
+        for account in accounts:
+            #对于每一个email都要找到其根，并且把email存入email_set对应的位置 
+            for email in account[1:]:
+                #这里不能用root_email = self.father[email],因为前面对email的union只是一个个的path(list)之间的union,
+                #而path与path之间还没有union,因此必须重新用find,才能找出每个email的root
+                root_email = self.find(email)
+                
+                #在email_to_id填入对应的名字
+                email_to_id[root_email] = account[0]
+                
+                if root_email not in email_set:
+                    email_set[root_email] = set()
+                email_set[root_email].add(email)
+        
+        return email_set, email_to_id
+    
+        
+    def initialize(self, accounts):
+        for account in accounts:
+            for email in account[1:]:
+                self.father[email] = email
+                
+                
+    def union(self, email_a, email_b):
+        root_a = self.find(email_a)
+        root_b = self.find(email_b)
+        
+        if root_a != root_b:
+            self.father[root_a] = root_b
+            
+            
+    def find(self, email):
+        path = []
+        
+        while self.father[email] != email:
+            path.append(email)
+            email = self.father[email]
+        
+        for p in path:
+            self.father[p] = email
+        
+        return email
