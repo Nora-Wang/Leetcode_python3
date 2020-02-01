ad98cb7cc79c7d869d13c7fc3f52d6ed0baea89f
Nora_Wang <ywang41@gwmail.gwu.edu>
2020-01-31 22:35:52 -0500

    Update 300. Longest Increasing Subsequence.py

--- Dynamic Programming/300. Longest Increasing Subsequence.py
+++ Dynamic Programming/300. Longest Increasing Subsequence.py
@@ -12,6 +12,8 @@ Your algorithm should run in O(n2) complexity.
 Follow up: Could you improve it to O(n log n) time complexity?
 
 
+坐标型DP
+    
 O(n2) 解法： 
 Dp[i] 表示以第i个数字为结尾的最长上升子序列的长度。 
 对于每个数字，枚举前面所有小于自己的数字 j，Dp[i] = max{Dp[j]} + 1. 如果没有比自己小的，Dp[i] = 1; 
@@ -36,3 +38,38 @@ class Solution:
         
         return max(dp)
         
+
+#输出path
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        # state: dp[i] 表示从左到右跳到i的最长sequence 的长度
+        
+        # initialization: dp[0..n-1] = 1
+        dp = [1] * len(nums)
+        
+        # prev[i] 代表 dp[i] 的最优值是从哪个 dp[j] 算过来的
+        prev = [-1] * len(nums)
+        
+        # function dp[i] = max{dp[j] + 1},  j < i and nums[j] < nums[i]
+        for i in range(len(nums)):
+            for j in range(i):
+                if nums[j] < nums[i] and dp[i] < dp[j] + 1:
+                    dp[i] = dp[j] + 1
+                    prev[i] = j
+        
+        # answer: max(dp[0..n-1])
+        longest, last = 0, -1
+        for i in range(len(nums)):
+            if dp[i] > longest:
+                longest = dp[i]
+                last = i
+        
+        path = []
+        while last != -1:
+            path.append(nums[last])
+            last = prev[last]
+        print(path[::-1])
+        
+        return longest
