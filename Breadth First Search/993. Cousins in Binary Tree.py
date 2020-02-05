614f904940f38e9509a6ea5b09cb72977c139048
Nora_Wang <ywang41@gwmail.gwu.edu>
2020-02-05 12:19:32 -0500

    Create 993. Cousins in Binary Tree.py

---
+++ Breadth First Search/993. Cousins in Binary Tree.py
@@ -0,0 +1,81 @@
+In a binary tree, the root node is at depth 0, and children of each depth k node are at depth k+1.
+
+Two nodes of a binary tree are cousins if they have the same depth, but have different parents.
+
+We are given the root of a binary tree with unique values, and the values x and y of two different nodes in the tree.
+
+Return true if and only if the nodes corresponding to the values x and y are cousins.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3,4], x = 4, y = 3
+Output: false
+Example 2:
+
+
+Input: root = [1,2,3,null,4,null,5], x = 5, y = 4
+Output: true
+Example 3:
+
+
+
+Input: root = [1,2,3,null,4], x = 2, y = 3
+Output: false
+ 
+
+Note:
+
+The number of nodes in the tree will be between 2 and 100.
+Each node has a unique integer value from 1 to 100.
+
+
+code:
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isCousins(self, root: TreeNode, x: int, y: int) -> bool:
+        queue = collections.deque([root])
+        flag_x, flag_y = False, False
+        
+        while queue:
+            #层级遍历
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                
+                if node.val == x:
+                    flag_x = True
+                if node.val == y:
+                    flag_y = True
+                
+                #保证x和y的parent不同
+                if node.left and node.right:
+                    #这里不能做加法/乘法之类的比较,因为可能存在情况:x=1,y=3,1+3 == 2+2
+                    if node.left.val == x and node.right.val == y:
+                        return False
+                    if node.left.val == y and node.right.val == x:
+                        return False
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            
+            #一层遍历结束后,发现x和y存在于同一层中,则返回True
+            if flag_x and flag_y:
+                return True
+            
+            #发现x或y单独存在于一层,则说明x和y不是cousin
+            if (flag_x and not flag_y) or (not flag_x and flag_y):
+                return False
+                
+    
+        
+        
