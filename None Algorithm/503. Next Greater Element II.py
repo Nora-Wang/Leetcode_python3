3b6a753ad9f5be7d5c4469ecac58b93770fea82a
Nora_Wang <ywang41@gwmail.gwu.edu>
2020-02-10 11:13:19 -0500

    Create 503. Next Greater Element II.py

---
+++ None Algorithm/503. Next Greater Element II.py
@@ -0,0 +1,33 @@
+Given a circular array (the next element of the last element is the first element of the array), print the Next Greater Number for every element. The Next Greater Number of a number x is the first greater number to its traversing-order next in the array, which means you could search circularly to find its next greater number. If it doesn't exist, output -1 for this number.
+
+Example 1:
+
+Input: [1,2,1]
+Output: [2,-1,2]
+Explanation: The first 1's next greater number is 2; 
+The number 2 can't find next greater number; 
+The second 1's next greater number needs to search circularly, which is also 2.
+Note: The length of given array won't exceed 10000.
+
+
+单调栈问题
+因为是环形,直接nums = nums + nums即可,result取前半部
+
+
+code:
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        stack = []
+        nums = nums + nums
+        results = [-1] * len(nums)
+        
+        
+        for i in range(len(nums)):
+            while stack and nums[stack[-1]] < nums[i]:
+                results[stack[-1]] = nums[i]
+                
+                stack.pop()
+        
+            stack.append(i)
+        
+        return results[:len(results) // 2]
