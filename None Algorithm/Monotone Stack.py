18d4de653f7aa56b0f07ea3e6aee79d09a0bd9e9
Nora_Wang <ywang41@gwmail.gwu.edu>
2020-02-10 10:07:27 -0500

    Update Monotone Stack.py

--- None Algorithm/Monotone Stack.py
+++ None Algorithm/Monotone Stack.py
@@ -3,6 +3,7 @@
 题目list = [num1, num2, ...], 从左往右看,求每个值之后第一个比该值大的数
 
 stack = []
+#若后续需要进行对结果的查找,即用num找结果:若num是数字,直接使用index即可;若num不是数字,则result设为hash table即可
 result = []
 
 for i in range(len(list)):
