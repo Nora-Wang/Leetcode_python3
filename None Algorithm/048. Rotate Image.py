0d24cdbf441127336bfc8aacc4a956cb25f15792
Nora_Wang <ywang41@gwmail.gwu.edu>
2020-02-17 09:52:18 -0500

    Create 048. Rotate Image.py

---
+++ None Algorithm/048. Rotate Image.py
@@ -0,0 +1,69 @@
+You are given an n x n 2D matrix representing an image.
+
+Rotate the image by 90 degrees (clockwise).
+
+Note:
+
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+Example 1:
+
+Given input matrix = 
+[
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+],
+
+rotate the input matrix in-place such that it becomes:
+[
+  [7,4,1],
+  [8,5,2],
+  [9,6,3]
+]
+Example 2:
+
+Given input matrix =
+[
+  [ 5, 1, 9,11],
+  [ 2, 4, 8,10],
+  [13, 3, 6, 7],
+  [15,14,12,16]
+], 
+
+rotate the input matrix in-place such that it becomes:
+[
+  [15,13, 2, 5],
+  [14, 3, 4, 1],
+  [12, 6, 8, 9],
+  [16, 7,10,11]
+]
+
+题目中说了不能另开matrix,因此只能在原有matrix基础上改变
+两步翻转:
+1.上下翻转
+2.对角线翻转
+
+
+code:
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        if not len(matrix) or not len(matrix[0]):
+            return []
+        
+        n = len(matrix)
+        
+        #上下翻转
+        for i in range(n // 2):
+            matrix[i], matrix[n-1-i] = matrix[n-1-i], matrix[i]
+        
+        #对角线翻转
+        for i in range(n):
+            for j in range(i,n):
+                if i == j:
+                    continue
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        
