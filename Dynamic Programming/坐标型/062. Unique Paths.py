96b46937619024546a028f3e721692b7ba8d2969
Nora_Wang <ywang41@gwmail.gwu.edu>
2020-02-18 14:17:28 -0500

    Update 062. Unique Paths.py

--- Dynamic Programming/坐标型/062. Unique Paths.py
+++ Dynamic Programming/坐标型/062. Unique Paths.py
@@ -45,16 +45,17 @@ code:
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         dp = [[0 for _ in range(m)] for _ in range(n)]
+        #第一个赋值为1
+        dp[0][0] = 1
         
         for i in range(n):
             for j in range(m):
-                if i == 0 or j == 0:
-                    dp[i][j] = 1
+                #当是左上角的值时,略过,否则后续会out of index
+                if i == 0 and j == 0:
                     continue
-                dp[i][j] = dp[i][j-1] + dp[i-1][j]
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
         
         return dp[n-1][m-1]
-
     
     
 递归解法:超时
