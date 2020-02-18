edc3167d482ef3046e80ca761a21a68bb5914e3d
Nora_Wang <ywang41@gwmail.gwu.edu>
2020-02-18 11:59:36 -0500

    Create 445. Add Two Numbers II.py

---
+++ ListNode/445. Add Two Numbers II.py
@@ -0,0 +1,42 @@
+You are given two non-empty linked lists representing two non-negative integers. The most significant digit comes first and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Follow up:
+What if you cannot modify the input lists? In other words, reversing the lists is not allowed.
+
+Example:
+
+Input: (7 -> 2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 8 -> 0 -> 7
+
+
+先转换为数字,相加后,转换为str,然后一个一个的放入dummy
+
+code:
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        dummy = tail = ListNode(None)
+        
+        num1, num2 = 0, 0
+        while l1:
+            num1 = num1 * 10 + l1.val
+            l1 = l1.next
+        while l2:
+            num2 = num2 * 10 + l2.val
+            l2 = l2.next
+        
+        res = num1 + num2
+        str_res = str(res)
+        for num in str_res:
+            tail.next = ListNode(int(num))
+            tail = tail.next
+        
+        return dummy.next
+        
