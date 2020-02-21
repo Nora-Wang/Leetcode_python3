f9cf7585dafd0d31a55c41092eb05963dd970283
Nora_Wang <ywang41@gwmail.gwu.edu>
2020-02-21 16:40:22 -0500

    Update 141. Linked List Cycle.py

--- Two Pointer/141. Linked List Cycle.py
+++ Two Pointer/141. Linked List Cycle.py
@@ -36,10 +36,13 @@ class Solution(object):
         :rtype: bool
         """
         slow = fast = head
+     
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
+         
             if fast == slow:
                 return True
+           
         return False
        
