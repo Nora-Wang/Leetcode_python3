bb1e0b23146f193700d48328779c423f707bb621
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2020-02-25 19:17:38 -0500

    Create 129. Sum Root to Leaf Numbers.py

---
+++ ListNode/129. Sum Root to Leaf Numbers.py
@@ -0,0 +1,65 @@
+Given a binary tree containing digits from 0-9 only, each root-to-leaf path could represent a number.
+
+An example is the root-to-leaf path 1->2->3 which represents the number 123.
+
+Find the total sum of all root-to-leaf numbers.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Input: [1,2,3]
+    1
+   / \
+  2   3
+Output: 25
+Explanation:
+The root-to-leaf path 1->2 represents the number 12.
+The root-to-leaf path 1->3 represents the number 13.
+Therefore, sum = 12 + 13 = 25.
+Example 2:
+
+Input: [4,9,0,5,1]
+    4
+   / \
+  9   0
+ / \
+5   1
+Output: 1026
+Explanation:
+The root-to-leaf path 4->9->5 represents the number 495.
+The root-to-leaf path 4->9->1 represents the number 491.
+The root-to-leaf path 4->0 represents the number 40.
+Therefore, sum = 495 + 491 + 40 = 1026.
+
+
+code:
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def sumNumbers(self, root: TreeNode) -> int:
+        self.res = 0
+        
+        self.calculate(root, 0)
+        
+        return self.res
+    
+    def calculate(self, root, prev):
+        if not root:
+            return prev
+        
+        #加入下一位
+        curt = root.val + prev * 10
+        
+        #当走到底的时候,将结果加入self.res
+        if not root.left and not root.right:
+            self.res += curt
+        
+        #计算左右子树的结果
+        self.calculate(root.left, curt)
+        self.calculate(root.right, curt)
