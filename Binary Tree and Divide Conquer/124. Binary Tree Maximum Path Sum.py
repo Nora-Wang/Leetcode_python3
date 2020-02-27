66bae50bffa95e3d8f76234fdfcde98b05964bf5
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2020-02-26 19:22:58 -0500

    Update 124. Binary Tree Maximum Path Sum.py

--- Binary Tree and Divide Conquer/124. Binary Tree Maximum Path Sum.py
+++ Binary Tree and Divide Conquer/124. Binary Tree Maximum Path Sum.py
@@ -15,6 +15,48 @@ Output: 6
 
 
 code:
+Version 1
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def maxPathSum(self, root: TreeNode) -> int:
+        self.res = root.val
+        
+        self.helper(root)
+        
+        return self.res
+    
+    def helper(self, root):
+        if not root:
+            return 0
+        
+        left = self.helper(root.left)
+        right = self.helper(root.right)
+        
+        #在root这个点得到最大值,与5个值做比较
+        #1. 与self.res自己做比较
+        #2. 与左边+root做比较,即左边线
+        #3. 与右边+root做比较,即右边线
+        #4. 与全部做比较,即从左边线到右边线的所有和
+        #5. 与root自己比较,因为存在左右两条边线的值都为负数的情况,这样返回root的值才是当前最大
+        self.res = max(self.res, left + right + root.val, left + root.val, right + root.val, root.val)
+        
+        #返回值相当于又要往上走,因此只用返回一条边线即可,因为存在左右两条边线的值都为负数的情况,因此还得与root自己比较
+        return max(left + root.val, right + root.val, root.val)
+       
+       
+       
+       
+       
+       
+       
+       
+Version 2
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
@@ -42,4 +84,4 @@ class Solution:
         
         curt = max(left_max, right_max) + root.val
         
-        return curt if curt > 0 else 0
+        return max(curt, 0)
