f7953739ceea41d0f1a80c5f20ccb983dde61aa8
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2020-03-09 22:39:42 -0400

    Update 104. Maximum Depth of Binary Tree.py

--- Binary Tree and Divide Conquer/104. Maximum Depth of Binary Tree.py
+++ Binary Tree and Divide Conquer/104. Maximum Depth of Binary Tree.py
@@ -18,6 +18,62 @@ Given binary tree [3,9,20,null,null,15,7],
 return its depth = 3.
 
 
+#Version 0
+#recursion
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        
+        left = self.maxDepth(root.left)
+        right = self.maxDepth(root.right)
+        
+        return max(left, right) + 1
+    
+    
+#Version 1
+#BFS
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        
+        queue = collections.deque([root])
+        
+        depth = 0
+        
+        while queue:
+            depth += 1
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        
+        return depth
+
+
+
+
+
+
+
 
 
 思路：
