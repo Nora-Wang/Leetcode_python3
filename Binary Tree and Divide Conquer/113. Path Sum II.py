d8b74ebb681b2fbd56cf75d78bc605afbdf78d5c
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2020-03-10 12:33:47 -0400

    Update 113. Path Sum II.py

--- Binary Tree and Divide Conquer/113. Path Sum II.py
+++ Binary Tree and Divide Conquer/113. Path Sum II.py
@@ -22,6 +22,39 @@ Return:
 
 
 
+Version by myself
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def pathSum(self, root: TreeNode, sum: int) -> List[List[int]]:
+        res = []
+        self.dfs(root, [], 0, res, sum)
+        
+        return res
+    
+    def dfs(self, root, path, curt, res, sum):
+        if not root:
+            return
+        
+        if not root.left and not root.right:
+            if curt + root.val == sum:
+                res_path = list(path) + [root.val]
+                res.append(res_path)
+            return
+        
+        path.append(root.val)
+        self.dfs(root.left, path, curt + root.val, res, sum)
+        self.dfs(root.right, path, curt + root.val, res, sum)
+        path.pop()
+
+
+
+
 code:
 # Definition for a binary tree node.
 # class TreeNode(object):
