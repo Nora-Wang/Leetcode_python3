f43e82aad33536fef6e1af7fa6e3cfefb574d70e
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2020-03-11 23:33:50 -0400

    Update 119. Pascal's Triangle II.py

--- Dynamic Programming/119. Pascal's Triangle II.py
+++ Dynamic Programming/119. Pascal's Triangle II.py
@@ -14,17 +14,50 @@ Follow up:
 Could you optimize your algorithm to use only O(k) extra space?
 
 
-坐标型DP + 滚动数组
+
 
 code:
+#坐标型DP
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        dp = [[1] * (i + 1) for i in range(rowIndex + 1)]
+        
+        for i in range(2, rowIndex + 1):
+            for j in range(1, len(dp[i]) - 1):
+                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j]
+        
+        return dp[-1]
+    
+    
+    
+Follow up 1: 滚动数组:所有的i/i-1 都%2
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
         dp = [[1] * (rowIndex + 1), [1] * (rowIndex + 1)]
         
         for i in range(2, rowIndex + 1):
+            #这里不再是dp的length了,而是i;注意也不是i+1,因为最后一位不需要计算
             for j in range(1, i):
-                dp[i % 2][j] = dp[i % 2 - 1][j] + dp[i % 2 - 1][j-1]
-
+                dp[i % 2][j] = dp[(i - 1) % 2][j - 1] + dp[(i - 1) % 2][j]
+                index = i
+        
+        #注意不再是取最后一行
         return dp[rowIndex % 2]
         
         
+Follow up 2: 给出制定坐标(a,b)的值
+class Solution:
+    def getRow(self, rowIndex) -> List[int]:
+        if a < 0 or b < 0 or b > a:
+            return None
+        
+        a = rowIndex
+        b = 3
+        
+        dp = [[1] * (a + 1), [1] * (a + 1)]
+        
+        for i in range(2, a + 1):
+            for j in range(1, i):
+                dp[i % 2][j] = dp[(i - 1) % 2][j] + dp[(i - 1) % 2][j - 1]
+        
+        print(dp[a % 2][b])
