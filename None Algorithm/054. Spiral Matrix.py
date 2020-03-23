99382f661cfde40db90a98e1ebd1aab22e2c3a50
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2020-03-22 21:15:18 -0400

    Update 054. Spiral Matrix.py

--- None Algorithm/054. Spiral Matrix.py
+++ None Algorithm/054. Spiral Matrix.py
@@ -20,6 +20,39 @@ Input:
 Output: [1,2,3,4,8,12,11,10,9,5,6,7]
 
 
+ 03/22/2020
+ class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        if not len(matrix) or not len(matrix[0]):
+            return []
+        
+        n = len(matrix)
+        m = len(matrix[0])
+        visited = set([(0,0)])
+        
+        self.res = [matrix[0][0]]
+        
+        self.dfs(matrix, visited, 0, 0)
+                    
+        return self.res
+                
+    def dfs(self, matrix, visited, x_, y_):
+        for direct in [(0,1),(1,0),(0,-1),(-1,0)] * (len(matrix) // 2 + 1):
+            while 0 <= (x_ + direct[0]) < len(matrix) and 0 <= (y_ + direct[1]) < len(matrix[0]) and (x_ + direct[0], y_ + direct[1]) not in visited:
+                x_ += direct[0]
+                y_ += direct[1]
+                
+                visited.add((x_,y_))
+                self.res.append(matrix[x_][y_])
+                
+        return
+                
+                
+ 
+ 
+ 
+ 
+ 
 对matrix四个方向的pop
 
 code:
