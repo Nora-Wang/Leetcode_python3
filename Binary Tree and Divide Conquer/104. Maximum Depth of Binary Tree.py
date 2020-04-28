c80dc93acfd63d3d39edbf619f4d83eb4e56e5fb
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2020-04-28 14:55:28 -0400

    Update 104. Maximum Depth of Binary Tree.py

--- Binary Tree and Divide Conquer/104. Maximum Depth of Binary Tree.py
+++ Binary Tree and Divide Conquer/104. Maximum Depth of Binary Tree.py
@@ -17,147 +17,64 @@ Given binary tree [3,9,20,null,null,15,7],
    15   7
 return its depth = 3.
 
-
-#Version 0
-#recursion
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
+'''
+brute force: walk through all path one by one from root to leaf, compare depth
+time: O(n^2), space: O(1)
 
-class Solution:
-    def maxDepth(self, root: TreeNode) -> int:
-        if not root:
-            return 0
-        
-        left = self.maxDepth(root.left)
-        right = self.maxDepth(root.right)
-        
-        return max(left, right) + 1
-    
-    
-#Version 1
+optimized: 
+BFS
+1. Analyze: traverse the tree level by level to find the depthest level -> use BFS (if root.left or root.right exist, append them to queue; utilize a for loop to saperate level)
+2. variables set: queue(for BFS), count(count the depth)
+3. edge case: root is None, count = 0, return
+4. time:O(n), space: O(1)
+
+Divide and conquer
+1. Analyze:
+1.1 base case: root is None, depth is 0
+1.2. recurtion rule: get the depthest length for root.left node and root.right node, get the max of two length and plus 1, that's the curt root's depthest length
+1.3. return value: return the curt root's depthest length
+
+2.variables set: left(the depthest length for root.left node), right(the depthest length for root.right node)
+
+3.edge case: root is None, depth is 0
+4.time: O(n), space:O(1)
+'''
+
+
 #BFS
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
 class Solution:
     def maxDepth(self, root: TreeNode) -> int:
         if not root:
             return 0
         
         queue = collections.deque([root])
-        
-        depth = 0
+        count = 0
         
         while queue:
-            depth += 1
+            count += 1
             for _ in range(len(queue)):
                 node = queue.popleft()
-                
                 if node.left:
                     queue.append(node.left)
                 if node.right:
                     queue.append(node.right)
         
-        return depth
-
-
-
-
-
-
-
-
-
-思路：
-利用divide conquer(dfs)思想，叶节点的下一层为0，往上依次加一，找出左右最大的depth
-
-
-
-
-code:
-leetcode version
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-class Solution(object):
-    def maxDepth(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
-        if not root:
-          #此处返回值为0，因为叶节点的下一层的depth为0，叶节点为1
-            return 0
-        #因为调用自身函数，因此需要用self.
-        leftDepth = self.maxDepth(root.left)
-        rightDepth = self.maxDepth(root.right)
-        result = max(leftDepth, rightDepth) + 1
-        
-        return result
-
+        return count
     
-lintcode version
-1. divide and conquer
-"""
-Definition of TreeNode:
-class TreeNode:
-    def __init__(self, val):
-        self.val = val
-        self.left, self.right = None, None
-"""
-
+    
+#Divide and Conquer
 class Solution:
-    """
-    @param root: The root of binary tree.
-    @return: An integer
-    """
-    def maxDepth(self, root):
+    def maxDepth(self, root: TreeNode) -> int:
         if not root:
             return 0
-            
+        
         left = self.maxDepth(root.left)
         right = self.maxDepth(root.right)
         
         return max(left, right) + 1
-
-2.traversal
-"""
-Definition of TreeNode:
-class TreeNode:
-    def __init__(self, val):
-        self.val = val
-        self.left, self.right = None, None
-"""
-
-class Solution:
-    """
-    @param root: The root of binary tree.
-    @return: An integer
-    """
-    def __init__(self):
-        self.depth = 0
-        
-    def maxDepth(self, root):
-        self.traverse(root, 1)
-        return self.depth
-        
-    def traverse(self, root, cur_depth):
-        if not root:
-            return
-        
-        self.depth = max(self.depth, cur_depth)
-        self.traverse(root.left, cur_depth + 1)
-        self.traverse(root.right, cur_depth + 1)
-        
