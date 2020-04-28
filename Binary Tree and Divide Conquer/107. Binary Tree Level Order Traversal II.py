cbf89938132f2017de2cd2f32d5ab9d571409b51
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2020-04-28 19:48:02 -0400

    Update and rename Breadth First Search/107. Binary Tree Level Order Traversal II.py to Binary Tree and Divide Conquer/107. Binary Tree Level Order Traversal II.py

--- Binary Tree and Divide Conquer/107. Binary Tree Level Order Traversal II.py
+++ Binary Tree and Divide Conquer/107. Binary Tree Level Order Traversal II.py
@@ -18,29 +18,36 @@ return its bottom-up level order traversal as:
 ]
 
 思路：
-同107，最后result.reverse()一下就行
+同107，最后reverse一下就行
 需要注意的是不能这样写：return result.reverse()，因为result.reverse()是一个使result翻转的函数调用，其本身是不返回值的，这样的结果是[]
+但可以写成return reversed(result)/ result[::-1]
 
-注意循环使用for _ in range(len_level):
-
-code: 
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
+'''
+brute force:
+use root.left and root.right to find the last level, and then find the second last level...
+time: O(n^2), space: O(n)
+
+optimized:
+1. analyze: level traversal -> BFS, if left/right exist, append it to queue
+2. variable set: queue(BFS), level(record nodes for curt level), stack_u_b(record levels from up to bottom)
+3. edge case: root is None, return empty list
+4. time: O(n), space: O(n)
+'''
+
 
 class Solution:
     def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
         if not root:
-            return[]
+            return []
         
-        return self.bfs(root)
-    
-    def bfs(self, root):
         queue = collections.deque([root])
-        result = []
+        stack_u_b = []
         
         while queue:
             level = []
@@ -53,6 +60,7 @@ class Solution:
                 if node.right:
                     queue.append(node.right)
             
-            result.append(level)
+            stack_u_b.append(level)
         
-        return result[::-1]
+        #return stack_u_b[::-1]
+        return reversed(stack_u_b)
