2e99d9d4ac0f8e6546cd682d4a843b39b67735cf
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2020-04-28 14:30:26 -0400

    Update and rename Breadth First Search/103. Binary Tree Zigzag Level Order Traversal.py to Binary Tree and Divide Conquer/103. Binary Tree Zigzag Level Order Traversal.py

--- Binary Tree and Divide Conquer/103. Binary Tree Zigzag Level Order Traversal.py
+++ Binary Tree and Divide Conquer/103. Binary Tree Zigzag Level Order Traversal.py
@@ -18,10 +18,6 @@ return its zigzag level order traversal as:
 ]
 
 
-思路：
-BFS模板，version1使用变量count记录level，偶数时用reverse翻转；version2使用变量rev记录level，为True时翻转
-
-code:
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
@@ -29,32 +25,48 @@ code:
 #         self.left = None
 #         self.right = None
 
+
+'''
+brute force: utilize the count of root.left and root.right to record level by level, and use a variable(True or False) to deside when need to invert the level
+
+optinized: traverse the tree level by level -> BFS -> use queue
+append node if node.left or node.right exist, use a for loop to pop node level by level
+use a variable(True or False) ot deside when to invert the level
+
+variables set: queue(for BFS), res(final result), level(record nodes level by level), invert(boolean, deside when reverse level)
+
+height: O(logn ~ n)
+time: O(n)
+space: O(n)
+edge case: root is None, return empty list
+'''
+
+
 class Solution:
     def zigzagLevelOrder(self, root: TreeNode) -> List[List[int]]:
         if not root:
             return []
         
         queue = collections.deque([root])
-        
-        result = []
-        flag = True
+        res = []
+        invert = False
         
         while queue:
             level = []
-
             for _ in range(len(queue)):
                 node = queue.popleft()
-
                 level.append(node.val)
                 
-                if node.right:
-                    queue.append(node.right)
                 if node.left:
                     queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
                 
-            if flag:
+            if invert:
                 level.reverse()
-            flag = not flag
-            result.append(level)
+            
+            res.append(level)
+            invert = not invert
         
-        return result
+        return res
+                    
