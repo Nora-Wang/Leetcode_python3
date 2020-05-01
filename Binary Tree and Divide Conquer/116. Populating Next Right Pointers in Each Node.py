db02a747f590c58a10a6f06c2a370e10d19b3f14
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2020-05-01 16:21:27 -0400

    Create 116. Populating Next Right Pointers in Each Node.py

---
+++ Binary Tree and Divide Conquer/116. Populating Next Right Pointers in Each Node.py
@@ -0,0 +1,90 @@
+You are given a perfect binary tree where all leaves are on the same level, and every parent has two children. The binary tree has the following definition:
+
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+Initially, all next pointers are set to NULL.
+
+ 
+
+Follow up:
+
+You may only use constant extra space.
+Recursive approach is fine, you may assume implicit stack space does not count as extra space for this problem.
+ 
+
+Example 1:
+
+
+
+Input: root = [1,2,3,4,5,6,7]
+Output: [1,#,2,3,#,4,5,6,7,#]
+Explanation: Given the above perfect binary tree (Figure A), your function should populate each next pointer to point to its next right node, just like in Figure B. The serialized output is in level order as connected by the next pointers, with '#' signifying the end of each level.
+ 
+
+Constraints:
+
+The number of nodes in the given tree is less than 4096.
+-1000 <= node.val <= 1000
+
+
+code:
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+'''
+brute force
+utilize left/right to iterate to find the next node
+time: O(n^2), space: O(n)
+
+optimize:
+#level traverse by BFS
+record all nodes by level to level, use a for loop to add the next node
+queue(BFS), record[level1, level2...]
+time: O(n), space: O(n)
+
+reduce space optimize:
+use a prev variable to record the previous node. if change level, prev = None. 
+if prev = None, prev = curt root; if prev exist, prev.next = curt root
+
+edge case: if not root, return None
+
+'''
+
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return None
+        
+        inital = root
+        queue = collections.deque([root])
+        
+        while queue:
+            prev = None
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                
+                if prev:
+                    prev.next = node
+                
+                prev = node
+                
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        
+        return root
