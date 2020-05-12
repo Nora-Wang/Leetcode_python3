404300afa12fce191814059e6f28907f5cbc4760
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2020-05-12 09:24:12 -0400

    Update 199. Binary Tree Right Side View.py

--- Binary Tree and Divide Conquer/199. Binary Tree Right Side View.py
+++ Binary Tree and Divide Conquer/199. Binary Tree Right Side View.py
@@ -19,6 +19,120 @@ Explanation:
 #         self.val = x
 #         self.left = None
 #         self.right = None
+
+
+#DFS Version
+'''
+DFS Version
+1. use recursion
+use recursion to level order traverse the tree, utilize depth
+1.1 really level order (from left to right), which means result is the last element in the record; use a list1[list2] to 
+record every level, index of list1 means the depth, list2 is the nodes in curt depth
+time: O(n), space: O(n)
+1.2 revised level order traverse, which means the result is the first element in the record; 
+if curt depth == len(record), which means curt root is the first node in the new depth(the most right one)
+time: O(n), space: O(1)
+
+2. use stack
+use stack to process the dfs for level order traverse, utilize depth
+2.1 really level order...
+2.2 revised level order...
+'''
+#DFS recursion 1.1
+class Solution:
+    def rightSideView(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+        
+        self.res = []
+        self.dfs(root, 0)
+        
+        return [level[-1] for level in self.res]
+    
+    def dfs(self, root, depth):
+        if not root:
+            return
+        
+        if depth >= len(self.res):
+            self.res.append([])
+        
+        self.res[depth].append(root.val)
+        
+        self.dfs(root.left, depth + 1)
+        self.dfs(root.right, depth + 1)
+
+#DFS recursion 1.2 revised
+class Solution:
+    def rightSideView(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+        
+        self.res = []
+        self.dfs(root, 0)
+        
+        return self.res
+    
+    def dfs(self, root, depth):
+        if not root:
+            return
+        
+        if depth == len(self.res):
+            self.res.append(root.val)
+        
+        self.dfs(root.right, depth + 1)
+        self.dfs(root.left, depth + 1)
+
+#DFS + Stack 1.1
+class Solution:
+    def rightSideView(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+        
+        stack = [(root, 0)]
+        res = []
+        
+        while stack:
+            node, depth = stack.pop()
+            
+            if depth == len(res):
+                res.append([])
+            
+            res[depth].append(node.val)
+            
+            #注意stack的特性，先进后出；这里要先append right，这样在stack pop的时候才会先pop出left，最后append进res的顺序才是left->right
+            if node.right:
+                stack.append((node.right, depth + 1))
+            if node.left:
+                stack.append((node.left, depth + 1))
+            
+        
+        return [level[-1] for level in res]
+
+#DFS + Stack 1.2 revised
+class Solution:
+    def rightSideView(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+        
+        stack = [(root, 0)]
+        res = []
+        
+        while stack:
+            node, depth = stack.pop()
+            
+            if depth == len(res):
+                res.append(node.val)
+            
+            if node.left:
+                stack.append((node.left, depth + 1))
+            if node.right:
+                stack.append((node.right, depth + 1))
+        
+        return res
+
+
+
+#BFS Version
 '''
 level traverse -> BFS -> queue
 final res append the last node in every level. the last node is last end node of the level for loop.
