855475474d7f36adc102af74bc1a6b740ad52bc0
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2020-05-24 20:12:05 -0400

    Create 416. Partition Equal Subset Sum.py

---
+++ Dynamic Programming/背包问题/01背包/416. Partition Equal Subset Sum.py
@@ -0,0 +1,68 @@
+Given a non-empty array containing only positive integers, find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.
+
+Note:
+
+Each of the array element will not exceed 100.
+The array size will not exceed 200.
+ 
+
+Example 1:
+
+Input: [1, 5, 11, 5]
+
+Output: true
+
+Explanation: The array can be partitioned as [1, 5, 5] and [11].
+ 
+
+Example 2:
+
+Input: [1, 2, 3, 5]
+
+Output: false
+
+Explanation: The array cannot be partitioned into equal sum subsets.
+
+解析：
+实际上就是求是否存在子数组，使得子数组的和等于整个数组和的1/2。考虑用动规，看成是0-1背包问题的一种变形，dp[i][j]表示考虑前i个数字，
+是否存在子数组和为j。
+
+转移方程为dp[i][j] = dp[i-1][j] | dp[i-1][j-nums[i]]（即不选第i个数字或者选第i个数字）
+
+可以用滚动数组进行优化。
+
+时间复杂度: O(MN). M是数组元素和，N是数组元素个数
+
+参考链接：https://www.acwing.com/solution/LeetCode/content/6416/
+
+code:
+'''
+step 1: set
+find a subset that sum is target(all_sum // 2)
+dp[i][j]: i means the previous i numbers; j means curt subset sum
+dp[i][j]表示考虑前i个数字，是否存在子数组和为j
+
+step 2: attribute
+count
+
+step 3:
+pick it or not pick it
+dp[i] -> dp[i - 1][j], dp[i - 1][j - nums[i]]
+'''
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        all_sum = sum(nums)
+        
+        if all_sum % 2:
+            return False
+        
+        target = all_sum // 2
+        
+        dp = [0] * (target + 1)
+        dp[0] = 1
+        
+        for i in range(0, len(nums)):
+            for j in range(target, nums[i] - 1, -1):
+                dp[j] |= dp[j - nums[i]]
+        
+        return dp[target]
