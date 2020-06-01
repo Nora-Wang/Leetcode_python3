e51130c8c324f272e5ed9d55fb41a54077c0de43
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2020-06-01 19:33:49 -0400

    Update 198. House Robber.py

--- Dynamic Programming/198. House Robber.py
+++ Dynamic Programming/198. House Robber.py
@@ -14,3 +14,28 @@ Input: [2,7,9,3,1]
 Output: 12
 Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
              Total amount you can rob = 2 + 9 + 1 = 12.
+
+    
+'''
+dp[i] means for curt house i, all the money the robber robbed
+
+dp[i][0] for curt i house, not rob
+dp[i][1] for curt i house, rob
+
+dp[i][0] = max(dp[i - 1][0], dp[i - 1][1])
+dp[i][1] = dp[i - 1][0] + nums[i] #if house i will rob -> house i - 1 cannot rob
+'''
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if not nums: 
+            return 0
+        
+        #dp的初始化。注意写法！！
+        dp = [[0,0] for _ in range(len(nums))]
+        dp[0][1] = nums[0]
+        
+        for i in range(1, len(nums)):
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1])
+            dp[i][1] = dp[i - 1][0] + nums[i]
+        
+        return max(dp[-1][0], dp[-1][1])
