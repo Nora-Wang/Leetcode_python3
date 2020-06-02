c87361b4de0f4824fbc34e4ebf961b4f3f0278e8
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2020-06-01 20:48:43 -0400

    Update 213. House Robber II.py

--- Dynamic Programming/213. House Robber II.py
+++ Dynamic Programming/213. House Robber II.py
@@ -14,3 +14,36 @@ Input: [1,2,3,1]
 Output: 4
 Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
              Total amount you can rob = 1 + 3 = 4.
+
+
+    
+'''
+same as house robberI, but saperate to two nums
+1. include first but not the last
+2. include the last house but not the first
+choose the max result
+
+edge cases:
+1. not nums
+2. only has one house -> after delete the first/last house, the nums is []
+'''
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        
+        if len(nums) == 1:
+            return nums[0]
+        
+        return max(self.helper(nums[1:]), self.helper(nums[:-1]))
+    
+    def helper(self, nums):
+        if not nums:
+            return 0
+        
+        rob, not_rob = nums[0], 0
+        
+        for i in range(1, len(nums)):
+            rob, not_rob = not_rob + nums[i], max(rob, not_rob)
+        
+        return max(rob, not_rob)    
