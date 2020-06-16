d5371d613d03f36d646e022d2636292433316786
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2020-06-16 15:21:11 -0400

    Update 210. Course Schedule II.py

--- Breadth First Search/Topological/210. Course Schedule II.py
+++ Breadth First Search/Topological/210. Course Schedule II.py
@@ -22,46 +22,86 @@ Explanation: There are a total of 4 courses to take. To take course 3 you should
              So one correct course order is [0,1,2,3]. Another correct ordering is [0,2,1,3] .
              
              
-             
+#BFS Version             
 主题思路跟207一模一样，只是I用count记录node个数，若与numCourses相同即符合
 而II则需要用result记录所遍历的courses，若其长度与numCourses相同即符合，然后输出result
-#corner case！！
-
-
-
-code:
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        graph, indegrees = self.create_graph_indegrees(numCourses, prerequisites)
-        
-        queue = collections.deque()
-        for course in indegrees:
-            if indegrees[course] == 0:
-                queue.append(course)
+        graph, neighbors = self.create(numCourses, prerequisites)
+        #graph = {pre_course:courses}
+        #neighbors = {course:how many pre_courses for this course}
         
         res = []
+        
+        queue = collections.deque()
+        for course in neighbors:
+            if neighbors[course] == 0:
+                queue.append(course)
+        
         while queue:
             course = queue.popleft()
             res.append(course)
-            
-            for next_course in graph[course]:
-                indegrees[next_course] -= 1
+            for sub_course in graph[course]:
+                neighbors[sub_course] -= 1
                 
-                if indegrees[next_course] == 0:
-                    queue.append(next_course)
+                if neighbors[sub_course] == 0:
+                    queue.append(sub_course)
         
-        #corner case 2
         return res if len(res) == numCourses else []
+    
+    def create(self, numCourses, prerequisites):
+        graph, neighbors = collections.defaultdict(list), {}
         
-        
-    def create_graph_indegrees(self, numCourses, prerequisites):
-        graph = collections.defaultdict(list)
-        indegrees = {i:0 for i in range(numCourses)}
-        
-        for curt, prev in prerequisites:
-            indegrees[curt] += 1
-            graph[prev].append(curt)
+        for course in range(numCourses):
+            neighbors[course] = 0
             
-        return graph, indegrees
+        for course, pre_course in prerequisites:
+            graph[pre_course].append(course)
+            neighbors[course] += 1
+        
+        return graph, neighbors
         
         
+#DFS Version
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        graph = collections.defaultdict(list)
+        for course, pre_course in prerequisites:
+            graph[pre_course].append(course)
+        
+        #0  means haven't been visited
+        #-1 means have been visited
+        #1  means visiting
+        visited = [0 for _ in range(numCourses)]
+        
+        #to record the visited courses
+        order = []
+        
+        for course in range(numCourses):
+            if self.dfs(graph, visited, course, order):
+                return []
+        
+        #because we go to the end course and pop it one by one, then add the courses into order, the order of courses are reversed
+        return reversed(order)
+    
+    def dfs(self, graph, visited, course, order):
+        #this course is in visiting list -> have circle -> end the recursion
+        if visited[course] == 1:
+            return True
+        #this course has been visited -> don't need to visite it anymore
+        if visited[course] == -1:
+            return False
+        
+        #mark this course as visiting
+        visited[course] = 1
+        for sub_course in graph[course]:
+            #if any sub_course for curt course have a circle -> end the recursion
+            if self.dfs(graph, visited, sub_course, order):
+                return True
+        
+        #mark this course as has been visited
+        visited[course] = -1
+        #add this course to order
+        order.append(course)
+        
+        return False
