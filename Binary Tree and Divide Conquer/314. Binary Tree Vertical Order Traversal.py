8e72ef30514f7d802794feaeedde9f495d949d86
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2020-07-01 19:22:48 -0400

    Update 314. Binary Tree Vertical Order Traversal.py

--- Binary Tree and Divide Conquer/314. Binary Tree Vertical Order Traversal.py
+++ Binary Tree and Divide Conquer/314. Binary Tree Vertical Order Traversal.py
@@ -106,38 +106,33 @@ class Solution:
       
       
 2. DFS
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+#DFS有个注意点：需要区分level，否则由于根左右的顺序，可能level大的node会先被append进temp
 class Solution:
     def verticalOrder(self, root: TreeNode) -> List[List[int]]:
         if not root:
             return []
         
-        record = collections.defaultdict(list)
+        hash_table = collections.defaultdict(list)
         
-        self.helper(root, 0, record, 0)
-        #print(record)
-        sorted_record = sorted(record.items(), key = lambda x : x[0])
-        #print(sorted_record)
+        self.helper(root, 0, 0, hash_table)
+        
+        #sort the hash_table by index
+        #[(index, (value, level)]
+        hash_table = sorted(hash_table.items(), key=lambda x:x[0])
+        
         res = []
-        for k, v in sorted_record:
-            v = sorted(v, key = lambda x:x[1])
-            curt = []
-            for value, row in v:
-                curt.append(value)
-            res.append(curt)
+        for _, value in hash_table:
+            #sort value by level
+            value.sort(key=lambda x:x[1])
+            temp = [v[0] for v in value]
+            res.append(temp)
         
         return res
-        
-        
-    def helper(self, root, index, record, row):
+    
+    def helper(self, root, index, level, hash_table):
         if not root:
             return
         
-        record[index].append((root.val, row))
-        self.helper(root.left, index - 1, record, row + 1)
-        self.helper(root.right, index + 1, record, row + 1)
+        hash_table[index].append((root.val, level))
+        self.helper(root.left, index - 1, level + 1, hash_table)
+        self.helper(root.right, index + 1, level + 1, hash_table)
