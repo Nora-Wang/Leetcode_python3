7f413c1cb6c8d7f29e15e7ea12aaec1ad9d7dcab
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2020-07-01 15:33:12 -0400

    Update and rename BST知识点 to Tree Summary.py

---
+++ Binary Tree and Divide Conquer/Tree Summary.py
@@ -0,0 +1,33 @@
+# 遍历顺序
+preorder traversal  根左右
+inorder traversal   左根右
+postorder traversal 左右根
+
+#题型分析
+1. 是前中后序遍历吗，是的话就用3序遍历的3种方法来做（遍历，栈，分治三种方法）
+2. 是层序遍历吗，是的话就bfs，用queue
+3. 是左右判断吗，是的话就分治法，但是又要分一下，是找点还是改点
+   找点left = self.helper
+   改点root.left = self.helper
+4. 是递归回溯法吗，是的话就dfs
+5. 硬解
+
+# Recursion & divide and conquer
+recursion和none recursion只是一种实现方式,而不是一种算法;traversal和divide and conquer都能用这两种方法实现
+divide and conquer与recursion的区别: divide and conquer一定有返回值,而recursion一般用全局变量/类变量代替返回值
+
+思想上的区别:
+recursion本质上是指操作重复,能无限调用同一函数对数据进行处理以得到结果
+traversal就是将整个tree的node给walk though一遍.主要用于判断
+divide and conquer却是从上往下的遍历所有node，然后再从下往上的return所需的返回值，最后在root处得到最终结果
+divide and conquer其实是一个postodrder,因为从下到上的实现
+
+
+
+#tips
+1. iteration == None recursion
+2. 每次divide and conquer的时候只要先写了not root,后续都可以直接写recursion root.left/root.right. 因为if not root,即保证了一定存在root.left/root.right
+3. Backtracking == DFS
+4. 当使用leaf节点时，一定要判断当前叶节点是否已经被加入temp中
+5. 一棵树的高度O(logn) ~ O(n)
+6. n个节点的binary tree一共有n个subtree(root,leaf都算)
