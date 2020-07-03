49b733647197f0182100b2fe947883093c71aa48
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2020-07-03 11:57:16 -0400

    Update Data Type.py

--- Python Notes/Data Type.py
+++ Python Notes/Data Type.py
@@ -1,41 +1,4 @@
-#Data Structure
-1. Array物理连续town house
-2. Stack逻辑概念可用别的物理概念来实现，先进后出
-3. Queue逻辑概念，先进先出
-4. Linked List利用物理地址
-5. Tree
-6. Heap
-7. Graph
-8. Hash Table
-
-
-
-#随机取值:import random
-1.random.choice(序列a):从序列a(str/list)中随机抽取一个元素
-2.random.sample(序列a，n):从序列a(str/list/set)中随机抽取n个元素，并将n个元素生以list形式返回;random.sample(序列a，n)[0]返回生成的第一个随机数
-3.random.randint(a,b):返回a到b的一个整数型随机数
-
-
-  
-#排序函数
-1.list.sort():仅对list,且返回的是对已经存在的列表进行操作，无返回值
-2.sorted(iterable):可以对所有可迭代的对象进行排序操作;注意其返回的是一个新的list,而不是在原来的基础上进行的操作
-  
-  
-#数据类型总结
-Python 中有六个标准的数据类型：Number（数字）, String（字符串）, List（列表）, Tuple（元组）, Set（集合）, Dictionary（字典）
-Python 的六个标准数据类型中：
-不可变数据（3 个）：Number（数字）、String（字符串）、Tuple（元组）
-#string需要注意:无法直接改变string中的其中一个值,一般先list(str),然后再进行改变,最后''.join(list_str)
-可变数据（3 个）：List（列表）、Dictionary（字典）、Set（集合）
-
-#数据结构
-heap, queue, stack
-
-
-dict和set查找/添加/删除key,时间复杂度为O(1)
-list,tuple,queue,stack查找/删除item,时间复杂度为O(n);tuple = ()为不可变的list
-***********************
+********************************************************************************************
 #dict和set中的key都是不可重复的,与list的index对应;但list中包含的值,以及dict的value都是可以重复的
 dict:
 1.删除key:
@@ -64,7 +27,7 @@ dict = collections.Counter(key的集合)
 dict中value为每个key出现的次数
 
 
-***********************
+********************************************************************************************
 set 没有value的dict:
 1.删除:
 set.remove(key)
@@ -79,11 +42,11 @@ set(item)结果为set('a','b')
 
 set([(i,j)])结果为set((i,j))
 
-***********************
-#两个list相加,相当于重建一个新list
-#list中的value值是可以重复的;与hash中的key做区别
+********************************************************************************************
+#两个list相加,相当于重建一个新list;可用extend来省空间
 #同一list中数据类型都是一样的
 #对list中的值进行重新赋值时,一定要用nums[index]的形式,不能直接num = item,这样无法成功赋值
+
 list:
 1.删除:
 #O(n)
@@ -107,7 +70,7 @@ list = [None for _ in range(n)]
 
 
 
-***********************
+********************************************************************************************
 ###queue和stack本质上是list
 queue(先进先出):
 1.初始化:
@@ -135,7 +98,7 @@ queue[0]
 7.队尾是最右,插入操作
 queue[-1]
 
-***********************
+********************************************************************************************
 stack(先进后出):
 1.初始化
 stack = []
@@ -151,6 +114,36 @@ stack[-1]
 5.栈底是最左
 stack[0]
 
-***********************
+********************************************************************************************
 tuple不可更改,但可以进行连接组合
 tup3 = tup1 + tup2
+
+
+
+
+
+********************************************************************************************
+#Data Structure
+1. Array物理连续town house
+2. Stack逻辑概念可用别的物理概念来实现，先进后出
+3. Queue逻辑概念，先进先出
+4. Linked List利用物理地址
+5. Tree
+6. Heap
+7. Graph
+8. Hash Table
+
+  
+#数据类型总结
+Python 中有六个标准的数据类型：Number（数字）, String（字符串）, List（列表）, Tuple（元组）, Set（集合）, Dictionary（字典）
+Python 的六个标准数据类型中：
+不可变数据（3 个）：Number（数字）、String（字符串）、Tuple（元组）
+#string需要注意:无法直接改变string中的其中一个值,一般先list(str),然后再进行改变,最后''.join(list_str)
+可变数据（3 个）：List（列表）、Dictionary（字典）、Set（集合）
+
+#数据结构
+heap, queue, stack
+
+
+dict和set查找/添加/删除key,时间复杂度为O(1)
+list,tuple,queue,stack查找/删除item,时间复杂度为O(n);tuple = ()为不可变的list
