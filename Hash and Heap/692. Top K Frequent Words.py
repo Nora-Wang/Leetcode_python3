01a257305863e668c98d71cf3b5f785f59f4d0d0
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2020-07-03 11:35:27 -0400

    Update 692. Top K Frequent Words.py

--- Hash and Heap/692. Top K Frequent Words.py
+++ Hash and Heap/692. Top K Frequent Words.py
@@ -23,6 +23,63 @@ Follow up:
 Try to solve it in O(n log k) time and O(n) extra space.
 
 
+
+
+#1. 直接用hash + sort的方式
+#time: O(nlogn), space: O(n)
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        if not words:
+            return []
+        
+        f_count = collections.Counter(words)
+        
+        #这个sort是按照frequency的逆序+word的增序排列的
+        sorted_w = sorted(f_count.items(), key=lambda x:(-x[1], x[0]))
+        
+        #一句话代表下面5行代码: return [sorted_w[i][0] for i in range(k if k <= len(sorted_w) else len(sorted_w))]
+        res = []
+        for i in range(k if k <= len(sorted_w) else len(sorted_w)):
+            res.append(sorted_w[i][0])
+            
+        return res
+
+
+#2. max heap: heapify all the hash_table, use max-heap to pop the top k one by one (or use nsmallest function.)
+#time: O(nlogn), space: O(n)
+'''
+这里如果用min heap会很麻烦，因为在排序的时候会先将frequency小的pop出去，问题是word order较小的也会被先pop，这与题目意思违背.而想要设置word较大的先被pop会很麻烦.
+因此采用frequency为负数 + word的排序方式，这样能保证heap在排序时是按照frequency较高 + word较小来排序的
+上面不用nlargest也是同样的道理.largest意味着frequency为正，但同时还要满足word较小的先被pop，不好实现.
+'''
+import heapq
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        if not words:
+            return []
+        
+        f_count = collections.Counter(words)
+        
+        #这里不好直接用heapify,因为heapify的对象必须是array,而items出来的结果为dict_items([('i', 2), ('love', 2), ('leetcode', 1), ('coding', 1)])
+        heap = []
+        for word, f in f_count.items():
+            heapq.heappush(heap, (-f, word))
+        
+        #一句话代表下面6行代码: return [v[1] for v in heapq.nsmallest(k if k <= len(sorted_w) else len(sorted_w), heap)]
+        res = []
+        while k and heap:
+            res.append(heapq.heappop(heap)[1])
+            k -= 1
+        
+        return res
+
+
+
+
+
+
+
+
 题目总体思路同347,主要就是sort部分
 code:
 class Solution(object):
@@ -42,6 +99,7 @@ class Solution(object):
         #sorted的时候先按照出现次数,然后按单词字母顺序排序
         #因此这里的key是-item[1]倒着排序的value值,即按照出现次数由大到小排序;item[0]正着排序的key值,即按照单词word的首字母顺序排序
         sorted_record = sorted(record.items(), key = lambda item:(-item[1], item[0]))
+        
         
         result = []
         for i in range(k):
