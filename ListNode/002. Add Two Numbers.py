3f8f48a9ea45038abc784507bc63af972368fd56
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2020-07-13 14:00:48 -0400

    Update 002. Add Two Numbers.py

--- ListNode/002. Add Two Numbers.py
+++ ListNode/002. Add Two Numbers.py
@@ -8,6 +8,37 @@ Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
 Output: 7 -> 0 -> 8
 Explanation: 342 + 465 = 807.
 
+    
+    
+#time: O(n1 + n2), space: O(1)
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        dummy = ListNode()
+        head = dummy
+        curt = 0
+        
+        while l1 or l2 or curt:
+            if l1:
+                curt += l1.val
+                l1 = l1.next
+            if l2:
+                curt += l2.val
+                l2 = l2.next
+                
+            head.next = ListNode(curt % 10)
+            head = head.next
+            
+            curt //= 10
+        
+        return dummy.next
+    
+    
+    
 
 
 code:
