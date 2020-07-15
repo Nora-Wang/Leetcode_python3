4f782997ac6fead7e2c969c1148875d87b383f6c
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2020-07-15 13:59:27 -0400

    Update 117. Populating Next Right Pointers in Each Node II.py

--- Binary Tree and Divide Conquer/117. Populating Next Right Pointers in Each Node II.py
+++ Binary Tree and Divide Conquer/117. Populating Next Right Pointers in Each Node II.py
@@ -90,27 +90,42 @@ referral:
 https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/discuss/37824/AC-Python-O(1)-space-solution-12-lines-and-easy-to-understand
 '''
 
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+# 思路：对于每一层从最左侧的node开始
+# 对于第一层来说使用curt来代表当前层的node
+# 利用来将curt的下一层所有的node连到一起：prev = sub_head = Node(None), prev.next等于curt的left/right，若prev.next存在则挪动prev，否则使用覆盖的原理寻找下一个node
+# curt = sub_head
+# time: O(n), space: O(1)
 class Solution:
     def connect(self, root: 'Node') -> 'Node':
-        record = root 
+        if not root:
+            return root
         
-        #loop all levels
-        while root:
-            curt = temp = Node(None)
-            #loop all nodes in curt level
-            #connect all nodes in a linkedlist
-            while root:
-                curt.next = root.left
-                if curt.next:
-                    curt = curt.next
-                curt.next = root.right
-                if curt.next:
-                    curt = curt.next
-
-                root = root.next
+        level_head = root
+        
+        while level_head:
+            curt = level_head
+            prev = sub_head = Node(None)
             
-            #there is a littel tricky, because temp means the dummy, and for the 102 row code -> temp = root.left 
-            #-> the first node in the next level
-            root = temp.next
+            while curt:
+                prev.next = curt.left
+                if prev.next:
+                    prev = prev.next
+                    
+                prev.next = curt.right
+                if prev.next:
+                    prev = prev.next
+                
+                curt = curt.next
             
-        return record
+            level_head = sub_head.next
+        
+        return root
