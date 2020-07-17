871aba3f443422f0ed89cc4ec107f83d0bc0d66e
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2020-07-17 14:57:10 -0400

    Update and rename Jiuzhang/028. Implement strStr().py to None Algorithm/028. Implement strStr().py

--- None Algorithm/028. Implement strStr().py
+++ None Algorithm/028. Implement strStr().py
@@ -12,6 +12,33 @@ Input: haystack = "aaaaa", needle = "bba"
 Output: -1
 
 
+# 07/17/2020
+# 记得问面试官：What should we return when needle is an empty string?
+# time: O((n - m) * m), space: O(1), n = l_h, m = l_n
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        l_h = len(haystack)
+        l_n = len(needle)
+        
+        # 题目要求
+        if not needle:
+            return 0
+        
+        # 当needle不为空，但haystack为空； or needle的长度大于haystack -> -1
+        if not haystack or l_h < l_n:
+            return -1
+        
+        for i in range(l_h - l_n + 1):
+            if haystack[i:i + l_n] == needle:
+                return i
+        
+        return -1
+    
+    
+    
+    
+    
+    
 注意此题的corner case的判断不需要另写,因为在for循环里面就可以包括
 思路:以len(needle)长度为标准,在haystack中寻找haystack[i:len(needle) + i]
 
