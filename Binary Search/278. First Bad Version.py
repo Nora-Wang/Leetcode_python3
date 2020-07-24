f649c9765848776a87dbc1396aa77ee4fe7ee17b
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2020-07-24 00:03:12 -0400

    Update 278. First Bad Version.py

--- Binary Search/278. First Bad Version.py
+++ Binary Search/278. First Bad Version.py
@@ -8,35 +8,28 @@ You are given an API bool isBadVersion(version) which will return whether versio
 完全的套模板
 
 code:
-
 # The isBadVersion API is already defined for you.
 # @param version, an integer
-# @return a bool
+# @return an integer
 # def isBadVersion(version):
 
-class Solution(object):
+class Solution:
     def firstBadVersion(self, n):
         """
         :type n: int
         :rtype: int
         """
-        if(n == 0):
-            return 1
-        start = 1
-        end = n
+        start, end = 1, n
+        
         while start + 1 < end:
-            mid = start + (end - start) / 2
-            #end和start就直接=mid，别管+1和-1
-            if(isBadVersion(mid)):
+            mid = (start + end) // 2
+            
+            if isBadVersion(mid):
                 end = mid
             else:
                 start = mid
-        #start和end的顺序要根据情况判断：求最小，先start；求最大，先end。
-        #此题因为现在相当于还剩4、5两个version，需要先判断4，若4是false，再判断5是否为true；
-        #若先判断5，则4也为true的情况会发生错误
-        if(isBadVersion(start)):
+        
+        if isBadVersion(start):
             return start
-        if(isBadVersion(end)):
-            return end
-        return 1
+        return end
 
