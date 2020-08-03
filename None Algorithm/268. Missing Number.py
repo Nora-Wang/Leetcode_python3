3d05edb2efff07e9b4f501fb8d7fce9ef1c17cbd
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2020-08-02 19:23:53 -0500

    Update 268. Missing Number.py

--- None Algorithm/268. Missing Number.py
+++ None Algorithm/268. Missing Number.py
@@ -12,10 +12,72 @@ Note:
 Your algorithm should run in linear runtime complexity. Could you implement it using only constant extra space complexity?
 
 
-一旦遇到list中数据与index有关的,都可以考虑将它们结合起来做,即将nums[abs(nums[i])] *= -1
+# 08/02/2020
+# Math Version
+# time: O(n), space: O(1)
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        
+        correct_sum = (0 + n) * (n + 1) // 2
+        
+        return correct_sum - sum(nums)
+    
+    
+# Sort Version
+# time: O(nlogn), space: O(1)
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        
+        for index, num in enumerate(nums):
+            if index != num:
+                return index
+        
+        return len(nums)
 
+# HashSet Version
+# time: O(n), space: O(n)
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        set_nums = set(nums)
+        
+        for i in range(len(nums) + 1):
+            if i not in set_nums:
+                return i
+        
+# List Index Version
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        
+        # 保证n也能被find，而不会out of index
+        nums.append(1)
+        
+        # 用于记录index：nums[index] = 0。因为0 * -1 = 0
+        mark_0 = 0
+        
+        # 排除最后一位，因为是自己加的
+        for i in range(len(nums) - 1):
+            # 当出现0，则mark位置，再将nums[0] * -1，以标记0在nums中出现过
+            if nums[i] == 0:
+                mark_0 = i
+            
+            nums[abs(nums[i])] *= -1
+        
+        for index, num in enumerate(nums):
+            if num > 0:
+                return index
+        
+        # 若所有数都标为负数了，则证明只有nums[index] = 0，即index这个值miss
+        return mark_0
+
+    
+    
 
 code:
+一旦遇到list中数据与index有关的,都可以考虑将它们结合起来做,即将nums[abs(nums[i])] *= -1
 #通用解法
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
