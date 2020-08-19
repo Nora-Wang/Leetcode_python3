4d9d58e7d7decdc59e43ca8fc68679dace7e6da4
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2020-08-19 14:32:40 -0500

    Update 020. Valid Parentheses.py

--- None Algorithm/020. Valid Parentheses.py
+++ None Algorithm/020. Valid Parentheses.py
@@ -27,6 +27,47 @@ Example 5:
 Input: "{[]}"
 Output: true
 
+    
+# 08/19/2020
+# time: O(n), space: O(n)
+# utilize stack's FILO
+# curt one and stack top: match -> one pair, pop; not match -> append into stack
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        
+        for char in s:
+            if not stack:
+                stack.append(char)
+                continue
+                
+            if stack[-1] == '(' and char == ')':
+                stack.pop()
+            elif stack[-1] == '[' and char == ']':
+                stack.pop()
+            elif stack[-1] == '{' and char == '}':
+                stack.pop()
+            else:
+                stack.append(char)
+        
+        return len(stack) == 0
+    
+# use hashtable to record the blackets
+# time: O(n), space: O(n)
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        brackets = {'}':'{', ']':'[', ')':'('}
+        
+        for char in s:
+            if stack and char in brackets and brackets[char] == stack[-1]:
+                stack.pop()
+            else:
+                stack.append(char)
+        
+        return len(stack) == 0
+    
+    
 Data Structure题
 题目考的就是对stack的理解与运用
 
