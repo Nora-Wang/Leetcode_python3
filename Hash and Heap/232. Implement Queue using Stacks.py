5c5471eadca709a9ed06b743006fb506399ff827
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2020-08-19 18:02:54 -0500

    Update 232. Implement Queue using Stacks.py

--- Hash and Heap/232. Implement Queue using Stacks.py
+++ Hash and Heap/232. Implement Queue using Stacks.py
@@ -20,55 +20,64 @@ Depending on your language, stack may not be supported natively. You may simulat
 You may assume that all operations are valid (for example, no pop or peek operations will be called on an empty queue).
 
 
-难点在pop出,利用python中stack的本质上就是一个list这一特性
-code:
-class MyQueue(object):
+# 因为queue是FIFO，而stack是FILO，因此在用stack表示queue的时候需要用两个stack
+# 第二个helper_stack主要用于queue的pop和找peek，因为queue在pop/找peek时应该是整个地址的bottom
+# 过程：先把stack一个一个的pop出，存入helper_stack，以得到queue的bottom；然后将helper_stack的内容重新放入stack中
+
+'''
+when we should use two stacks to implement a queue?
+The application for this implementation is to separate read & write of a queue in multi-processing. One of the stack is for read, and another is for write. 
+They only interfere each other when the former one is full or latter is empty.
+'''
+class MyQueue:
 
     def __init__(self):
         """
         Initialize your data structure here.
         """
         self.stack = []
+        self.helper_stack = []
 
-    def push(self, x):
+    def push(self, x: int) -> None:
         """
         Push element x to the back of queue.
-        :type x: int
-        :rtype: None
         """
         self.stack.append(x)
 
-    def pop(self):
+    def pop(self) -> int:
         """
         Removes the element from in front of queue and returns that element.
-        :rtype: int
         """
+        self.move_out()
+        res = self.stack.pop()
+        self.move_back()
         
-        node = self.stack[0]
-        
-        #Version 1
-        self.stack = self.stack[1:]
-        #Version 2
-        self.stack.remove(node)
-        
-        return node
-        
-        
+        return res
 
-    def peek(self):
+    def peek(self) -> int:
         """
         Get the front element.
-        :rtype: int
         """
-        return self.stack[0]
+        self.move_out()
+        peek = self.stack.pop()
+        self.stack.append(peek)
+        self.move_back()
         
+        return peek
 
-    def empty(self):
+    def empty(self) -> bool:
         """
         Returns whether the queue is empty.
-        :rtype: bool
         """
         return len(self.stack) == 0
+    
+    def move_out(self) -> None:
+        while len(self.stack) > 1:
+            self.helper_stack.append(self.stack.pop())
+    
+    def move_back(self) -> None:
+        while self.helper_stack:
+            self.stack.append(self.helper_stack.pop())
 
 
 # Your MyQueue object will be instantiated and called as such:
