445408681c36bea57751c706b2fbad41828cbf87
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2020-08-26 18:13:53 -0500

    Update 083. Easy_Remove Duplicates from Sorted List.py

--- ListNode/083. Easy_Remove Duplicates from Sorted List.py
+++ ListNode/083. Easy_Remove Duplicates from Sorted List.py
@@ -15,35 +15,26 @@ Output: 1->2->3
 设置一个ListNode curt = head的头指针
 若curt.val = curt.next.val则将curt的指针指向curt.next.next，即将curt.next这个node从head中删掉
 
-###第二次做没考虑[]的情况。两种方法。
-当head = None，后面的curt.next不成立，所以必须提前判断head = None的情况
-
-code:
 
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
-class Solution(object):
-    def deleteDuplicates(self, head):
-        """
-        :type head: ListNode
-        :rtype: ListNode
-        """
-###第一种方法
-        if(head == None):
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# time: O(n), space: O(1)
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        if not head:
             return None
+        
         curt = head
-  ###第二年种方法
-        #while(curt and curt.next)
-        while(curt.next != None):
-            if(curt.val != curt.next.val):
-                curt = curt.next
-            else:
+        
+        while curt.next:
+            if curt.val == curt.next.val:
                 curt.next = curt.next.next
-            
+            else:
+                curt = curt.next
+        
         return head
         
         
