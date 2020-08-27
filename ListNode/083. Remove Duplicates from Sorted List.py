25bf7a8e2a003fc3de80fd7f29b50a47eea10b19
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2020-08-26 19:49:51 -0500

    Update 083. Remove Duplicates from Sorted List.py

--- ListNode/083. Remove Duplicates from Sorted List.py
+++ ListNode/083. Remove Duplicates from Sorted List.py
@@ -15,6 +15,17 @@ Output: 1->2->3
 设置一个ListNode curt = head的头指针
 若curt.val = curt.next.val则将curt的指针指向curt.next.next，即将curt.next这个node从head中删掉
 
+'''
+易错case：
+1. if curt.val != prev.val:
+        prev.next = curt
+        prev = prev.next
+            
+   curt = curt.next
+   出现case: [1,1,2,3,3] -> [1,2,3,3]
+2. 若要使用dummy，需要注意dummy node的val的设置;因为head.val可能会任意值(0, -1)
+'''
+
 
 # Definition for singly-linked list.
 # class ListNode:
