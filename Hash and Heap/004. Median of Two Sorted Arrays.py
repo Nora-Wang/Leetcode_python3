3a14389c16dc6192ab60da043a7b91ca597d3cd1
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2020-09-07 20:32:13 -0500

    Update 004. Median of Two Sorted Arrays.py

--- Hash and Heap/004. Median of Two Sorted Arrays.py
+++ Hash and Heap/004. Median of Two Sorted Arrays.py
@@ -17,10 +17,54 @@ nums2 = [3, 4]
 
 The median is (2 + 3)/2 = 2.5
 
+# 1. brute force: two pointer
+# 使用双指针分别对两个数组遍历，比较两个指针下的元素大小，每次移动更小的指针，通过移动次数确定中位数的位置。
+# time: O(n + m), space: O(1)
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        p_1, p_2 = 0, 0
+        
+        # 对于odd奇数个来说，k的结果是指第k的数是median (eg: 一共7个数，k = 4)
+        # 对于even偶数个来说，k的结果是指第k的数是larger_median，第k-1的数是smaller_median (eg: 一共6个数，k = 4)
+        k = (len(nums1) + len(nums2)) // 2 + 1
+        # 使用larger_median就是为了记录median的值，便于最后输出，不然不知道最后一步是更新的p_1还是p_2 -> 无法知道median到底是nums1[p_1] 还是 nums2[p_2]
+        larger_median = None
+        
+        for _ in range(k):
+            # 这里使用smaller_median：当偶数个数据时，需要得到两个median
+            # 前面得到的k保证了最后循环结束得到的是larger_median，而整个算法是是使用two pointer一个一个数值找的，那么最后找到larger_median的上一个数据一定是smaller_median
+            smaller_median = larger_median
+            
+            # p_1 and p_2 are both in the index range
+            if p_1 < len(nums1) and p_2 < len(nums2):
+                if nums1[p_1] < nums2[p_2]:
+                    larger_median = nums1[p_1]
+                    p_1 += 1
+                else:
+                    larger_median = nums2[p_2]
+                    p_2 += 1
+            
+            # p_1 or p_2 out of index
+            elif p_1 < len(nums1):
+                larger_median = nums1[p_1]
+                p_1 += 1
+            else:
+                larger_median = nums2[p_2]
+                p_2 += 1
+        
+        # 奇数
+        if (len(nums1) + len(nums2)) % 2:
+            return larger_median
+        # 偶数
+        return (larger_median + smaller_median) / 2
 
-基本上就是用最小堆与最大堆求中位数的模板
+# 2. binary search
 
-code:
+# time: O(log(n + m)), space: O(1)
+
+
+
+# 3. Heap: 基本上就是用最小堆与最大堆求中位数的模板
 import heapq
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
