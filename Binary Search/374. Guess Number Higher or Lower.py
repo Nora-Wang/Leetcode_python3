1acf1cecb4b5144387bd09d5fe99a8930f462e15
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2020-10-13 09:48:53 -0500

    Create 374. Guess Number Higher or Lower.py

---
+++ Binary Search/374. Guess Number Higher or Lower.py
@@ -0,0 +1,63 @@
+We are playing the Guess Game. The game is as follows:
+
+I pick a number from 1 to n. You have to guess which number I picked.
+
+Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
+
+You call a pre-defined API int guess(int num), which returns 3 possible results:
+
+-1: The number I picked is lower than your guess (i.e. pick < num).
+1: The number I picked is higher than your guess (i.e. pick > num).
+0: The number I picked is equal to your guess (i.e. pick == num).
+Return the number that I picked.
+
+ 
+
+Example 1:
+
+Input: n = 10, pick = 6
+Output: 6
+Example 2:
+
+Input: n = 1, pick = 1
+Output: 1
+Example 3:
+
+Input: n = 2, pick = 1
+Output: 1
+Example 4:
+
+Input: n = 2, pick = 2
+Output: 2
+ 
+
+Constraints:
+
+1 <= n <= 231 - 1
+1 <= pick <= n
+
+二分模板
+
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+# def guess(num: int) -> int:
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        left, right = 1, n
+        
+        while left + 1 < right:
+            mid = (left + right) // 2
+            
+            curt_res = guess(mid)
+            
+            if curt_res == 0:
+                return mid
+            
+            if curt_res == 1:
+                left = mid
+            else:
+                right = mid
+        
+        return left if guess(left) == 0 else right
