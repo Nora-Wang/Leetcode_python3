6ecbe845af4b7c7d999555df4d4caebb975bdebe
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2020-11-16 16:15:16 -0600

    Update 340. Longest Substring with At Most K Distinct Characters.py

--- Two Pointer/340. Longest Substring with At Most K Distinct Characters.py
+++ Two Pointer/340. Longest Substring with At Most K Distinct Characters.py
@@ -12,6 +12,48 @@ Output: 2
 Explanation: T is "aa" which its length is 2.
 
 
+'''
+window + hashtable
+
+while right < len(s):
+    1. create avaliable window[left:right] -> at most has k distinct chars or curt char is the same as previous char in window
+        hash_s = {char:freq}
+    2. renew res
+    3. move window -> move left, renew distinct and hash_s
+
+time: O(n), space: O(n)
+'''
+class Solution:
+    def lengthOfLongestSubstringKDistinct(self, s: str, k: int) -> int:
+        if not s or k < 1:
+            return 0
+        
+        hash_s = collections.defaultdict(int)
+        res = 0
+        left, right = 0, 0
+        
+        while right < len(s):
+            # create window
+            while right < len(s) and (len(hash_s) < k or s[right] in hash_s):
+                hash_s[s[right]] += 1
+                right += 1
+            
+            # renew res
+            res = max(res, right - left)
+            
+            # move window
+            hash_s[s[left]] -= 1
+            if hash_s[s[left]] == 0:
+                del hash_s[s[left]]
+            left += 1
+        
+        return res
+            
+            
+    
+    
+    
+    
 跟159一样,都是同向双指针问题,就是要多一个对k的判定条件
 
 code:
