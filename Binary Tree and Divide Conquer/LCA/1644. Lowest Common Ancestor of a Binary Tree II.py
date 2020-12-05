9317a1f11c36a26e96fdd38cd4aae5ed2a7aa30d
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2020-12-05 13:10:55 -0600

    Update 1644. Lowest Common Ancestor of a Binary Tree II.py

--- Binary Tree and Divide Conquer/LCA/1644. Lowest Common Ancestor of a Binary Tree II.py
+++ Binary Tree and Divide Conquer/LCA/1644. Lowest Common Ancestor of a Binary Tree II.py
@@ -32,14 +32,72 @@ The number of nodes in the tree is in the range [1, 104].
 -109 <= Node.val <= 109
 All Node.val are unique.
 p != q
+Follow up: Can you find the LCA traversing the tree, without checking nodes existence?
+ 
+Version 1
+# 利用Leetcode 236
+# 不同点：It is NOT guaranteed that both p and q are in the tree.
+# -> 1. 需要判断p、q是否出现
+# -> 2. We need to traverse the entire tree even after we've found one of them. 
+#       而Leetcode 236则是we can return either p OR q as soon as we find one of them以判断是否p、q同时存在
+#       原因：If you return early, the above example would be null, because the code stops when it finds 5 and does not keep searching for 4.
+
+Version 1 # Use boolean as flags
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        self.has_p = False
+        self.has_q = False
+        res = self.helper(root, p, q)
+        
+        return res if self.has_p and self.has_q else None
+    
+    def helper(self, root, p, q):
+        if not root:
+            return root
+        
+        left = self.helper(root.left, p, q)
+        right = self.helper(root.right, p, q)
+        
+        # 不同点2 -> 需要在最后进行判断，不能在recursion之前
+        if root == p:
+            self.has_p = True
+            return root
+        if root == q:
+            self.has_q = True
+            return root
+        
+        if left and right:
+            return root
+        
+        return left if left else right
+
+Version 2 # Use integers as flags
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        count = [0]
+        res = self.helper(root, p, q, count)
+        
+        return res if count[0] == 2 else None
+    
+    def helper(self, root, p, q, count):
+        if not root:
+            return root
+        
+        left = self.helper(root.left, p, q, count)
+        right = self.helper(root.right, p, q, count)
+        
+        # 同样要放recursion的后面
+        if root == p or root == q:
+            count[0] += 1
+            return root
+        
+        if left and right:
+            return root
+        
+        return left if left else right
  
  
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+Version 0 # 自己做的方法，思路类似is balanced tree，同Version 1 Use boolean as flags
 # 判断p和q是否有出现
 # 因为用的divide and conquer，即down-top，一旦出现满足p和q同时出现，就用self.res记录，则能找到LCA
 # time: O(n), space: O(n)
@@ -71,4 +129,5 @@ class Solution:
  
  
 
-Follow up: Can you find the LCA traversing the tree, without checking nodes existence?
+
+
