6714fa1d69cdce31e52d9f41c0d093fe3e4f06a2
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2020-12-08 22:17:39 -0600

    Create Chapter5_2.py

---
+++ Bit Manipulation/Chapter5_2.py
@@ -0,0 +1,6 @@
+Given a (decimal - e g 3 72) number that is passed in as a string, print the binary rep- resentation If the number can not be represented accurately in binary, 
+print “ERROR”
+
+
+
+
