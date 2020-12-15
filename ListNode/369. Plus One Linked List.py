5f8dd15ecebc1277f444f918ef296c00b3f1724e
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2020-12-15 14:43:45 -0600

    Create 369. Plus One Linked List.py

---
+++ ListNode/369. Plus One Linked List.py
@@ -0,0 +1,44 @@
+Given a non-negative integer represented as a linked list of digits, plus one to the integer.
+
+The digits are stored such that the most significant digit is at the head of the list.
+
+ 
+
+Example 1:
+
+Input: head = [1,2,3]
+Output: [1,2,4]
+Example 2:
+
+Input: head = [0]
+Output: [1]
+ 
+
+Constraints:
+
+The number of nodes in the linked list is in the range [1, 100].
+0 <= Node.val <= 9
+The number represented by the linked list does not contain leading zeros except for the zero itself. 
+
+# time: O(n), space: O(1)
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def plusOne(self, head: ListNode) -> ListNode:
+        num = 0
+        while head:
+            num = num * 10 + head.val
+            head = head.next
+        
+        num += 1
+        head = None
+        while num:
+            node = ListNode(num % 10)
+            node.next = head
+            head = node
+            num //= 10
+        
+        return head
