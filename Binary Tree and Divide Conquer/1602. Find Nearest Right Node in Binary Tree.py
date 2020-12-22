a01e5f5a5e2715cba12c3a1c3e5365334525e044
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2020-12-22 10:02:13 -0600

    Create 1602. Find Nearest Right Node in Binary Tree.py

---
+++ Binary Tree and Divide Conquer/1602. Find Nearest Right Node in Binary Tree.py
@@ -0,0 +1,69 @@
+Given the root of a binary tree and a node u in the tree, return the nearest node on the same level that is to the right of u, or return null if u is the rightmost node in its level.
+
+ 
+
+Example 1:
+
+
+
+Input: root = [1,2,3,null,4,5,6], u = 4
+Output: 5
+Explanation: The nearest node on the same level to the right of node 4 is node 5.
+Example 2:
+
+
+
+Input: root = [3,null,4,2], u = 2
+Output: null
+Explanation: There are no nodes to the right of 2.
+Example 3:
+
+Input: root = [1], u = 1
+Output: null
+Example 4:
+
+Input: root = [3,4,2,null,null,null,1], u = 4
+Output: 2
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 105].
+1 <= Node.val <= 105
+All values in the tree are distinct.
+u is a node in the binary tree rooted at root.
+
+
+
+# BFS
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findNearestRightNode(self, root: TreeNode, u: TreeNode) -> TreeNode:
+        if not root:
+            return None
+        
+        queue = collections.deque([(root, 0)])
+        u_depth = -1
+        while queue:
+            node, depth = queue.popleft()
+            
+            if u_depth != -1 and u_depth == depth:
+                return node
+            
+            if node == u:
+                u_depth = depth
+            
+            if node.left:
+                queue.append((node.left, depth + 1))
+            if node.right:
+                queue.append((node.right, depth + 1))
+        
+        return None
+        
+
+
