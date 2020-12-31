18ee75d64438dfc6ae16e888a44333c19d87a74c
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2020-12-30 23:50:59 -0600

    Update 206. Reverse Linked List.py

--- ListNode/206. Reverse Linked List.py
+++ ListNode/206. Reverse Linked List.py
@@ -37,7 +37,7 @@ null <- [1,]  [2,] -> [3,] -> [4,] -> [,5,]
 
 
 code:
-
+# Iteration
 # Definition for singly-linked list.
 # class ListNode(object):
 #     def __init__(self, x):
@@ -62,6 +62,30 @@ class Solution(object):
             curt = temp
          
         return prev
+
+# recursion
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
+            return head
+        
+        # reverse from head.next to tail -> 两段LinkedList：new_head -> ... -> head.next + head -> head.next
+        new_head = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+
+        return new_head
+       
+       
+       
+       
+       
        
        
        
