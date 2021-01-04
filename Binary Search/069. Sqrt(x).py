2b8ee0639f5d197ae043e8a62aa6530074d64491
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2021-01-04 12:42:33 -0600

    Update 069. Sqrt(x).py

--- Binary Search/069. Sqrt(x).py
+++ Binary Search/069. Sqrt(x).py
@@ -6,9 +6,39 @@
 #Since the return type is an integer, the decimal digits are truncated and only the integer part of the result is returned.
 
 
+# 1/4/2020
+# Version 1: Binary Search
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        start, end = 0, x 
 
-#Question:
-#为什么不能是result >= x or result < x????
+        while start + 1 < end:
+            mid = (start + end) // 2
+
+            if mid**2 == x:
+                return mid
+            if mid**2 < x:
+                start = mid
+            else:
+                end = mid
+                
+        return end if end**2 <= x else start
+    
+# Version 2: recursion + bit manipulation   
+# sqrt(x) = 2 * sqrt(x // 4)
+# x << y = x * 2^y
+# x >> y = x // 2^y
+# -> sqrt(x) = sqrt(x >> 2) << 1
+# time: O(logn), space: O(logn) for recursion
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x < 2:
+            return x
+        
+        left = self.mySqrt(x >> 2) << 1
+        right = left + 1 # eg: x = 2, left = 1, right = 2
+        
+        return left if right**2 > x else right
 
 
 
