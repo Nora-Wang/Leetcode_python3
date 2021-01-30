6cc7f5b164ba0c04fbde6c3432495dcc3809be95
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2021-01-29 21:42:00 -0600

    Update 300. Longest Increasing Subsequence.py

--- Dynamic Programming/坐标型/300. Longest Increasing Subsequence.py
+++ Dynamic Programming/坐标型/300. Longest Increasing Subsequence.py
@@ -14,11 +14,10 @@ Follow up: Could you improve it to O(n log n) time complexity?
 
 坐标型DP
     
-O(n2) 解法： 
+# time: O(n^2), space: O(n)
 Dp[i] 表示以第i个数字为结尾的最长上升子序列的长度。 
 对于每个数字，枚举前面所有小于自己的数字 j，Dp[i] = max{Dp[j]} + 1. 如果没有比自己小的，Dp[i] = 1; 
     
-code:
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         if not nums:
@@ -28,7 +27,7 @@ class Solution:
         dp = [1] * len(nums)
         
         #遍历nums里的每一个点
-        for i in range(1,len(nums)):
+        for i in range(len(nums)):
             #遍历当前点之前的所有点 
             for j in range(i):
                 #如果之前点的值小于当前点的值，就要考虑加入dp 
