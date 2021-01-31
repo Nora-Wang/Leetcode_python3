b4d43d7c613bcbc9a58e99d45dc5c4299c753cec
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2021-01-31 13:56:00 -0600

    Update 031. Next Permutation.py

--- None Algorithm/031. Next Permutation.py
+++ None Algorithm/031. Next Permutation.py
@@ -11,6 +11,44 @@ Here are some examples. Inputs are in the left-hand column and its corresponding
 1,1,5 → 1,5,1
 
 
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        if len(nums) <= 1:
+            return
+        
+        # 找到第一个比后面数要小的数的index
+        index = len(nums) - 2
+        while index >= 0 and nums[index] >= nums[index + 1]:
+            index -= 1
+        
+        # nums单调递减的情况
+        if index == -1:
+            nums.sort()
+            return
+        
+        # 在后面的数中找到与nums[index]最近且更大的数
+        swap_index = len(nums) - 1
+        while nums[swap_index] <= nums[index]:
+            swap_index -= 1
+        nums[swap_index], nums[index] = nums[index], nums[swap_index]
+        
+        # 将index之后的数据进行排序（这部分的数据应为单减的，因此直接头尾swap即可）
+        left, right = index + 1, len(nums) - 1
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
+        
+        return
+
+
+
+
+
+
 code:
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
