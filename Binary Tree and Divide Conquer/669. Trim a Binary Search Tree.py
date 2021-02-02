3fb848d745b0edbcc4a0c364e474db6202cd029a
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2021-02-02 10:41:32 -0600

    Create 669. Trim a Binary Search Tree.py

---
+++ Binary Tree and Divide Conquer/669. Trim a Binary Search Tree.py
@@ -0,0 +1,65 @@
+Given the root of a binary search tree and the lowest and highest boundaries as low and high, trim the tree so that all its elements lies in [low, high]. Trimming the tree should not change the relative structure of the elements that will remain in the tree (i.e., any node's descendant should remain a descendant). It can be proven that there is a unique answer.
+
+Return the root of the trimmed binary search tree. Note that the root may change depending on the given bounds.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,0,2], low = 1, high = 2
+Output: [1,null,2]
+Example 2:
+
+
+Input: root = [3,0,4,null,2,null,null,1], low = 1, high = 3
+Output: [3,2,null,1]
+Example 3:
+
+Input: root = [1], low = 1, high = 2
+Output: [1]
+Example 4:
+
+Input: root = [1,null,2], low = 1, high = 3
+Output: [1,null,2]
+Example 5:
+
+Input: root = [1,null,2], low = 2, high = 4
+Output: [2]
+ 
+
+Constraints:
+
+The number of nodes in the tree in the range [1, 104].
+0 <= Node.val <= 104
+The value of each node in the tree is unique.
+root is guaranteed to be a valid binary search tree.
+0 <= low <= high <= 104
+
+'''
+从high level的角度思考：
+1. 当前root的值 < low -> 直接返回trim(root.right)的结果
+2. 当前root的值 > high -> 直接返回trim(root.left)的结果
+3. 当前root的值符合要求 -> 处理一下root.left 和 root.right
+
+time: O(n), space: O(1)
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def trimBST(self, root: TreeNode, low: int, high: int) -> TreeNode:
+        if not root:
+            return None
+        
+        if root.val < low:
+            return self.trimBST(root.right, low, high)
+        elif root.val > high:
+            return self.trimBST(root.left, low, high)
+        else:
+            root.left = self.trimBST(root.left, low, high)
+            root.right = self.trimBST(root.right, low, high)
+            return root
