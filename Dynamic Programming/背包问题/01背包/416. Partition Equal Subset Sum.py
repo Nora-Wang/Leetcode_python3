20bcb0a1298e86dbd3e09184ee5b751dc9b3f9be
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2024-01-17 22:47:18 +0800

    Update 416. Partition Equal Subset Sum.py

--- Dynamic Programming/背包问题/01背包/416. Partition Equal Subset Sum.py
+++ Dynamic Programming/背包问题/01背包/416. Partition Equal Subset Sum.py
@@ -90,3 +90,40 @@ class Solution:
             return True
         
         return self.help(nums, index + 1, target) or self.help(nums, index + 1, target - nums[index])
+
+
+'''
+二维DP写法
+
+实际上就是求是否存在子数组，使得子数组的和等于整个数组和的1/2。考虑用动规，看成是0-1背包问题的一种变形，dp[i][j]表示考虑前i个数字，
+是否存在子数组和为j。
+
+转移方程为dp[i][j] = dp[i-1][j] | dp[i-1][j-nums[i]]（即不选第i个数字或者选第i个数字）
+
+时间复杂度: O(MN). M是数组元素和，N是数组元素个数
+空间复杂度: O(MN)
+
+参考链接：https://www.acwing.com/solution/LeetCode/content/6416/
+'''
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        total = sum(nums)
+    
+        if total % 2 != 0:
+            return False
+        
+        target = total // 2
+        
+        dp = [[False for _ in range(target + 1)] for _ in range(len(nums) + 1)]
+        
+        for i in range(len(nums) + 1):
+            dp[i][0] = True
+        
+        for i in range(1, len(nums) + 1):
+            for j in range(1, target + 1):
+                if j - nums[i - 1] >= 0:
+                    dp[i][j] = dp[i - 1][j] or dp[i - 1][j - nums[i - 1]]
+                else:
+                    dp[i][j] = dp[i - 1][j]
+        
+        return dp[len(nums)][target]
