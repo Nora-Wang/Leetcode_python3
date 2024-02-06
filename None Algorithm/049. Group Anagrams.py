92361ffb4275161afbbf2cb2213acd3c7dea3ce0
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2024-02-06 15:06:27 +0800

    Update 049. Group Anagrams.py

--- None Algorithm/049. Group Anagrams.py
+++ None Algorithm/049. Group Anagrams.py
@@ -15,6 +15,19 @@ All inputs will be in lowercase.
 The order of your output does not matter.
 
 
+
+# 2024/02/06
+# 一次for循环
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hash_s = collections.defaultdict(list)
+        for s in strs:
+            sorted_s = ''.join(sorted(s))
+            hash_s[sorted_s].append(s)
+        
+        return hash_s.values()
+
+
 思路:
 用hash解决问题,重点是key的选取:用排序后的word作为key,将原始word作为value加入到dict中
 
