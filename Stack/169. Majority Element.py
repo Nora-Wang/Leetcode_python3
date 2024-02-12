1b3d67787626b4f16daed985bad5bfff2ff0c466
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2024-02-12 11:33:29 +0800

    Update 169. Majority Element.py

--- Stack/169. Majority Element.py
+++ Stack/169. Majority Element.py
@@ -19,29 +19,48 @@ class Solution:
     
     
 #摩尔选举
-#摩尔投票算法的基本原理关于摩尔投票算法的基本原理可以大致归纳为以下几步（为了方便理解，使用较为通俗语言进行描述）： 1.  假设第一个数字是候选数字，计数为 1 。 2.  遍历后面的数字，如果遇到相同的数字，计数加 1 ；如果遇到不同的数字，计数减 1 。 3.  当计数减到0时，将当前数字设为新的候选数字，计数重新设为 1 。 4.  最后剩下的候选数字很有可能是出现次数最多的数字。
+#摩尔投票算法的基本原理关于摩尔投票算法的基本原理可以大致归纳为以下几步（为了方便理解，使用较为通俗语言进行描述）： 
+# 1.  假设第一个数字是候选数字，计数为 1 。 
+# 2.  遍历后面的数字，如果遇到相同的数字，计数加 1 ；如果遇到不同的数字，计数减 1 。 
+# 3.  当计数减到0时，将当前数字设为新的候选数字，计数重新设为 1 。 
+# 4.  最后剩下的候选数字很有可能是出现次数最多的数字。
 
 # 作者：力扣（LeetCode）
 # 链接：https://www.zhihu.com/question/49973163/answer/3202021480
 # 来源：知乎
 # 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+# 完全按照上面解释的code
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        if not nums:
-            return []
+        cur = nums[0]
+        count = 1
         
-        count = 0
-        
-        for num in nums:
+        for i in range(1, len(nums)):
             if count == 0:
-                curt = num
+                cur = nums[i]
                 count = 1
-                
-            elif curt == num:
+            elif nums[i] == cur:
                 count += 1
-                
             else:
                 count -= 1
         
-        return curt
+        return cur
+        
+# 简化后的code
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = 0
+        cur = 0
+        
+        for num in nums:
+            if count == 0:
+                cur = num
+                
+            if num == cur:
+                count += 1
+            else:
+                count -= 1
+        
+        return cur
     
