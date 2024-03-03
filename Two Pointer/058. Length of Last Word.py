42e9f0e6d791fb4a075117cc6131e1c344c8e88c
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2024-03-03 11:48:41 +0800

    Update and rename 58. Length of Last Word.py to 058. Length of Last Word.py

--- Two Pointer/058. Length of Last Word.py
+++ Two Pointer/058. Length of Last Word.py
@@ -57,3 +57,18 @@ class Solution:
             slow = fast
         
         return len(word)
+
+# Improved Two Pointer
+# Start from the end to find the last word
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        end = len(s) - 1
+        while end >= 0 and s[end] == ' ':
+            end -= 1
+            
+        start = end
+        while start >= 0 and s[start] != ' ':
+            start -= 1
+
+        return end - start
+        
