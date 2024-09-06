8f3a19044be75030b139c754bb9d179ca6ee5298
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2024-09-06 18:29:30 +0800

    Create 3217. Delete Nodes From Linked List Present in Array.py

---
+++ ListNode/3217. Delete Nodes From Linked List Present in Array.py
@@ -0,0 +1,78 @@
+You are given an array of integers nums and the head of a linked list. Return the head of the modified linked list after removing all nodes from the linked list that have a value that exists in nums.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3], head = [1,2,3,4,5]
+
+Output: [4,5]
+
+Explanation:
+
+
+
+Remove the nodes with values 1, 2, and 3.
+
+Example 2:
+
+Input: nums = [1], head = [1,2,1,2,1,2]
+
+Output: [2,2,2]
+
+Explanation:
+
+
+
+Remove the nodes with value 1.
+
+Example 3:
+
+Input: nums = [5], head = [1,2,3,4]
+
+Output: [1,2,3,4]
+
+Explanation:
+
+
+
+No node has value 5.
+
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 105
+All elements in nums are unique.
+The number of nodes in the given list is in the range [1, 105].
+1 <= Node.val <= 105
+The input is generated such that there is at least one node in the linked list that has a value not present in nums.
+
+
+
+# Time O(n), Space O(1)
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def modifiedList(self, nums: List[int], head: Optional[ListNode]) -> Optional[ListNode]:
+        nums_set = set(nums)
+        
+        new_head = ListNode()
+        new_head.next = head
+        prev, cur = new_head, new_head.next
+        
+        while cur:
+            if cur.val not in nums_set:
+                prev.next = cur
+                prev = cur
+
+            cur = cur.next
+        
+        prev.next = None
+            
+        return new_head.next
