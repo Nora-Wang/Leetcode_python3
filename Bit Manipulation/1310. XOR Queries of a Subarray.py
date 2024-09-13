8950aebe8f14643d673e54cf04c3e2a63a0f6577
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2024-09-13 14:23:46 +0800

    Create 1310. XOR Queries of a Subarray.py

---
+++ Bit Manipulation/1310. XOR Queries of a Subarray.py
@@ -0,0 +1,55 @@
+You are given an array arr of positive integers. You are also given the array queries where queries[i] = [lefti, righti].
+
+For each query i compute the XOR of elements from lefti to righti (that is, arr[lefti] XOR arr[lefti + 1] XOR ... XOR arr[righti] ).
+
+Return an array answer where answer[i] is the answer to the ith query.
+
+ 
+
+Example 1:
+
+Input: arr = [1,3,4,8], queries = [[0,1],[1,2],[0,3],[3,3]]
+Output: [2,7,14,8] 
+Explanation: 
+The binary representation of the elements in the array are:
+1 = 0001 
+3 = 0011 
+4 = 0100 
+8 = 1000 
+The XOR values for queries are:
+[0,1] = 1 xor 3 = 2 
+[1,2] = 3 xor 4 = 7 
+[0,3] = 1 xor 3 xor 4 xor 8 = 14 
+[3,3] = 8
+Example 2:
+
+Input: arr = [4,8,2,10], queries = [[2,3],[1,3],[0,0],[0,3]]
+Output: [8,0,4,4]
+ 
+
+Constraints:
+
+1 <= arr.length, queries.length <= 3 * 104
+1 <= arr[i] <= 109
+queries[i].length == 2
+0 <= lefti <= righti < arr.length
+
+
+# https://leetcode.com/problems/xor-queries-of-a-subarray/discuss/5778700/Beats-100-Explained-with-Video-C%2B%2BJavaPythonJS-Prefix-XOR-Explained-in-Detail
+class Solution:
+    def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
+        pre = [0] * len(arr)
+        pre[0] = arr[0]
+        
+        for i in range(1, len(arr)):
+            pre[i] = pre[i - 1] ^ arr[i]
+        
+        result = []
+        for left, right in queries:
+            if left == 0:
+                result.append(pre[right])
+            else:
+                result.append(pre[left - 1] ^ pre[right])
+        
+        return result
+        
