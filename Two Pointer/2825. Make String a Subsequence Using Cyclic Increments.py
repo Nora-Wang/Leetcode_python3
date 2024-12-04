e9f90598a9d74fb7e0b3862cfb4b4b1172cf76ca
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2024-12-04 11:35:31 +0800

    Create 2825. Make String a Subsequence Using Cyclic Increments.py

---
+++ Two Pointer/2825. Make String a Subsequence Using Cyclic Increments.py
@@ -0,0 +1,98 @@
+You are given two 0-indexed strings str1 and str2.
+
+In an operation, you select a set of indices in str1, and for each index i in the set, increment str1[i] to the next character cyclically. That is 'a' becomes 'b', 'b' becomes 'c', and so on, and 'z' becomes 'a'.
+
+Return true if it is possible to make str2 a subsequence of str1 by performing the operation at most once, and false otherwise.
+
+Note: A subsequence of a string is a new string that is formed from the original string by deleting some (possibly none) of the characters without disturbing the relative positions of the remaining characters.
+
+ 
+
+Example 1:
+
+Input: str1 = "abc", str2 = "ad"
+Output: true
+Explanation: Select index 2 in str1.
+Increment str1[2] to become 'd'. 
+Hence, str1 becomes "abd" and str2 is now a subsequence. Therefore, true is returned.
+Example 2:
+
+Input: str1 = "zc", str2 = "ad"
+Output: true
+Explanation: Select indices 0 and 1 in str1. 
+Increment str1[0] to become 'a'. 
+Increment str1[1] to become 'd'. 
+Hence, str1 becomes "ad" and str2 is now a subsequence. Therefore, true is returned.
+Example 3:
+
+Input: str1 = "ab", str2 = "d"
+Output: false
+Explanation: In this example, it can be shown that it is impossible to make str2 a subsequence of str1 using the operation at most once. 
+Therefore, false is returned.
+ 
+
+Constraints:
+
+1 <= str1.length <= 105
+1 <= str2.length <= 105
+str1 and str2 consist of only lowercase English letters.
+
+# n = len(str1), m = len(str2)
+# Time O(max(n,m)), Space O(1)
+  
+# Map
+class Solution:
+    def canMakeSubsequence(self, str1: str, str2: str) -> bool:
+        charMap = collections.defaultdict()
+        charMap['a'] = 'b';
+        charMap['b'] = 'c';
+        charMap['c'] = 'd';
+        charMap['d'] = 'e';
+        charMap['e'] = 'f';
+        charMap['f'] = 'g';
+        charMap['g'] = 'h';
+        charMap['h'] = 'i';
+        charMap['i'] = 'j';
+        charMap['j'] = 'k';
+        charMap['k'] = 'l';
+        charMap['l'] = 'm';
+        charMap['m'] = 'n';
+        charMap['n'] = 'o';
+        charMap['o'] = 'p';
+        charMap['p'] = 'q';
+        charMap['q'] = 'r';
+        charMap['r'] = 's';
+        charMap['s'] = 't';
+        charMap['t'] = 'u';
+        charMap['u'] = 'v';
+        charMap['v'] = 'w';
+        charMap['w'] = 'x';
+        charMap['x'] = 'y';
+        charMap['y'] = 'z';
+        charMap['z'] = 'a';
+        
+        index = 0
+        for char in str1:
+            if char == str2[index] or charMap[char] == str2[index]:
+                index += 1
+            
+            if index == len(str2):
+                return True
+        
+        return False
+
+
+# List
+class Solution:
+    def canMakeSubsequence(self, str1: str, str2: str) -> bool:
+        charList = ['b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a']
+        
+        index = 0
+        for char in str1:
+            if char == str2[index] or charList[ord(char) - ord('a')] == str2[index]:
+                index += 1
+            
+            if index == len(str2):
+                return True
+        
+        return False
