977c4aee216667c86ecd2bc382c35e9db3fcb6ba
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2025-05-19 21:19:30 +0800

    Create 3024. Type of Triangle.py

---
+++ None Algorithm/3024. Type of Triangle.py
@@ -0,0 +1,62 @@
+You are given a 0-indexed integer array nums of size 3 which can form the sides of a triangle.
+
+A triangle is called equilateral if it has all sides of equal length.
+A triangle is called isosceles if it has exactly two sides of equal length.
+A triangle is called scalene if all its sides are of different lengths.
+Return a string representing the type of triangle that can be formed or "none" if it cannot form a triangle.
+
+ 
+
+Example 1:
+
+Input: nums = [3,3,3]
+Output: "equilateral"
+Explanation: Since all the sides are of equal length, therefore, it will form an equilateral triangle.
+Example 2:
+
+Input: nums = [3,4,5]
+Output: "scalene"
+Explanation: 
+nums[0] + nums[1] = 3 + 4 = 7, which is greater than nums[2] = 5.
+nums[0] + nums[2] = 3 + 5 = 8, which is greater than nums[1] = 4.
+nums[1] + nums[2] = 4 + 5 = 9, which is greater than nums[0] = 3. 
+Since the sum of the two sides is greater than the third side for all three cases, therefore, it can form a triangle.
+As all the sides are of different lengths, it will form a scalene triangle.
+ 
+
+Constraints:
+
+nums.length == 3
+1 <= nums[i] <= 100
+
+
+# Brute force with sorted
+class Solution:
+    def triangleType(self, nums: List[int]) -> str:
+        nums.sort()
+
+        x, y, k = nums[0], nums[1], nums[2]
+        if x + y <= k:
+            return 'none'
+        if x == y and y == k:
+            return 'equilateral'
+        if (x == y and y != k) or (x != y and y == k):
+            return 'isosceles'
+        if x != y and y != k and x != k:
+            return 'scalene'
+        
+# Optimation with HashSet
+class Solution:
+    def triangleType(self, nums: List[int]) -> str:
+        nums.sort()
+        nums_set = set(nums)
+
+        if nums[0] + nums[1] <= nums[2]:
+            return 'none'
+        
+        if len(nums_set) == 1:
+            return 'equilateral'
+        if len(nums_set) == 2:
+            return 'isosceles'
+        
+        return 'scalene'
