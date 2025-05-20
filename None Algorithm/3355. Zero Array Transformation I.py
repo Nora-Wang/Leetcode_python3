7b58b95f45aec5cf783ce7e37e6140c0f6cf3628
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2025-05-20 17:50:27 +0800

    Create 3355. Zero Array Transformation I.py

---
+++ None Algorithm/3355. Zero Array Transformation I.py
@@ -0,0 +1,66 @@
+You are given an integer array nums of length n and a 2D array queries, where queries[i] = [li, ri].
+
+For each queries[i]:
+
+Select a subset of indices within the range [li, ri] in nums.
+Decrement the values at the selected indices by 1.
+A Zero Array is an array where all elements are equal to 0.
+
+Return true if it is possible to transform nums into a Zero Array after processing all the queries sequentially, otherwise return false.
+
+ 
+
+Example 1:
+
+Input: nums = [1,0,1], queries = [[0,2]]
+
+Output: true
+
+Explanation:
+
+For i = 0:
+Select the subset of indices as [0, 2] and decrement the values at these indices by 1.
+The array will become [0, 0, 0], which is a Zero Array.
+Example 2:
+
+Input: nums = [4,3,2,1], queries = [[1,3],[0,2]]
+
+Output: false
+
+Explanation:
+
+For i = 0:
+Select the subset of indices as [1, 2, 3] and decrement the values at these indices by 1.
+The array will become [4, 2, 1, 0].
+For i = 1:
+Select the subset of indices as [0, 1, 2] and decrement the values at these indices by 1.
+The array will become [3, 1, 0, 0], which is not a Zero Array.
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+0 <= nums[i] <= 105
+1 <= queries.length <= 105
+queries[i].length == 2
+0 <= li <= ri < nums.length
+
+# diff的构造，很精妙
+class Solution:
+    def isZeroArray(self, nums: List[int], queries: List[List[int]]) -> bool:
+        if max(nums) > len(queries):
+            return False
+
+        diff = [0] * len(nums)
+        for l,r in queries:
+            diff[l] += 1
+            if r + 1 < len(nums):
+                diff[r + 1] -= 1
+        
+        count = 0
+        for i in range(len(nums)):
+            count += diff[i]
+            if count < nums[i]:
+                return False
+        
+        return True
