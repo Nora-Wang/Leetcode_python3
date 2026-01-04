29394725565f047f32ce258242a4a140b8736b60
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2026-01-04 14:33:19 +0800

    Update 083. Remove Duplicates from Sorted List.py

--- ListNode/083. Remove Duplicates from Sorted List.py
+++ ListNode/083. Remove Duplicates from Sorted List.py
@@ -11,6 +11,56 @@ Example 2:
 Input: 1->1->2->3->3
 Output: 1->2->3
 
+
+# 01/04/2026
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return head
+            
+        dummy = ListNode()
+        dummy.next = head
+
+        cur, head = dummy.next, head.next
+
+        while head:
+            while head and head.val == cur.val:
+                head = head.next
+
+            cur.next = head
+            cur = cur.next
+
+            if head:
+                head = head.next
+        
+        return dummy.next
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return head
+
+        cur = head
+        while cur.next:
+            if cur.val == cur.next.val:
+                cur.next = cur.next.next
+            else:
+                cur = cur.next
+            
+        return head
+
+
 思路：
 设置一个ListNode curt = head的头指针
 若curt.val = curt.next.val则将curt的指针指向curt.next.next，即将curt.next这个node从head中删掉
