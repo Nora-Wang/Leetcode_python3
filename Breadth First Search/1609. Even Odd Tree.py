5eff22037b561fde0deea2b128e69a239aa71e1e
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2026-01-06 14:35:08 +0800

    Create 1609. Even Odd Tree.py

---
+++ Breadth First Search/1609. Even Odd Tree.py
@@ -0,0 +1,89 @@
+A binary tree is named Even-Odd if it meets the following conditions:
+
+The root of the binary tree is at level index 0, its children are at level index 1, their children are at level index 2, etc.
+For every even-indexed level, all nodes at the level have odd integer values in strictly increasing order (from left to right).
+For every odd-indexed level, all nodes at the level have even integer values in strictly decreasing order (from left to right).
+Given the root of a binary tree, return true if the binary tree is Even-Odd, otherwise return false.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,10,4,3,null,7,9,12,8,6,null,null,2]
+Output: true
+Explanation: The node values on each level are:
+Level 0: [1]
+Level 1: [10,4]
+Level 2: [3,7,9]
+Level 3: [12,8,6,2]
+Since levels 0 and 2 are all odd and increasing and levels 1 and 3 are all even and decreasing, the tree is Even-Odd.
+Example 2:
+
+
+Input: root = [5,4,2,3,3,7]
+Output: false
+Explanation: The node values on each level are:
+Level 0: [5]
+Level 1: [4,2]
+Level 2: [3,3,7]
+Node values in level 2 must be in strictly increasing order, so the tree is not Even-Odd.
+Example 3:
+
+
+Input: root = [5,9,1,3,5,7]
+Output: false
+Explanation: Node values in the level 1 should be even integers.
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 105].
+1 <= Node.val <= 106
+
+
+# 01/06/2026
+# Time O(n), Space O(n)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isEvenOddTree(self, root: Optional[TreeNode]) -> bool:
+        queue = collections.deque([root])
+        depth = 0
+
+        while queue:
+            temp = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                temp.append(node.val)
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            
+            if depth % 2:
+                if not self.isEvenDecreasing(temp):
+                    return False
+            else:
+                if not self.isOddIncreasing(temp):
+                    return False
+
+            depth += 1
+        return True
+    
+    def isEvenDecreasing(self, temp):
+        for i in range(len(temp) - 1):
+            if temp[i] <= temp[i + 1] or temp[i] % 2:
+                return False
+        return not temp[-1] % 2
+    
+    def isOddIncreasing(self, temp):
+        for i in range(len(temp) - 1):
+            if temp[i] >= temp[i + 1] or not temp[i] % 2:
+                return False
+        return temp[-1] % 2
