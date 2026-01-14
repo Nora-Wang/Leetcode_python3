e7a793965c01b7a97e9ea182f7ba2dfb64a56524
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2026-01-14 14:20:42 +0800

    Update 101. Symmetric Tree.py

--- Binary Tree and Divide Conquer/101. Symmetric Tree.py
+++ Binary Tree and Divide Conquer/101. Symmetric Tree.py
@@ -22,6 +22,31 @@ Note:
 Bonus points if you could solve it both recursively and iteratively.
 
 
+# 2026/01/14
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+            
+        return self.helper(root.left, root.right)
+
+    def helper(self, left, right):
+        if not left and not right:
+            return True
+        
+        if not left or not right:
+            return False
+        
+        return left.val == right.val and self.helper(left.left, right.right) and self.helper(left.right, right.left)
+
+
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
