3f76eb82493159d469a1ba8c45a0a221c6065baf
Nora_Wang <43252941+Nora-Wang@users.noreply.github.com>
2026-01-15 12:32:39 +0800

    Update 098. Validate Binary Search Tree.py

--- Binary Tree and Divide Conquer/098. Validate Binary Search Tree.py
+++ Binary Tree and Divide Conquer/098. Validate Binary Search Tree.py
@@ -17,6 +17,26 @@ Example 1:
 Input: [2,1,3]
 Output: true
 
+# 2026/01/15
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        return self.helper(root, float('-inf'), float('inf'))
+    
+    def helper(self, root, lower, higher):
+        if not root:
+            return True
+        
+        if not (lower < root.val < higher):
+            return False
+        
+        return self.helper(root.left, lower, root.val) and self.helper(root.right, root.val, higher)
+
 
 #07/01/2020
 class Solution:
